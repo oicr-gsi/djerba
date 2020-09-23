cc249aedf47435f9475da126a5942502716e957f
alexjfortuna <alexjfortuna@gmail.com>
2020-09-23 17:11:17 -0400

    add expected list to testing framework

--- prototypes/tmb_test.py
+++ prototypes/tmb_test.py
@@ -17,8 +17,11 @@ class TestSampleMetrics(unittest.TestCase):
         maf_path = os.path.join(self.data_dir, 'tmb', 'somatic.maf.txt.gz')
         bed_path = os.path.join(self.data_dir, 'tmb', 'S31285117_Regions.bed')
         tcga_path = os.path.join(self.data_dir, 'tmb', 'tcga_tmbs.txt')
-        expected_path = os.path.join(self.data_dir, 'tmb', 'tcga_tmbs.txt')
-        expected =
+        cancer_type = "blca"
+        expected_tmb = 3.0236235709599097
+        expected_tmb_pct = 0.5156686317095873
+        expected_tmb_cohort_pct = 0.17603911980440098
+        expected = [expected_tmb, expected_tmb_pct, expected_tmb_cohort_pct]
         output = find_tmb(maf_path, bed_path, tcga_path, cancer_type)
         for i in range(len(output)):
             self.assertEqual(output[i], expected[i])
