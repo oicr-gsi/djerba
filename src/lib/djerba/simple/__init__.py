3f1ab7a607af5029919bd144f8c7a1109ea43174
iainrb <1695061+iainrb@users.noreply.github.com>
2021-06-23 15:44:12 -0400

    Move 'simple' classes and data to main Djerba package

--- src/lib/djerba/simple/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Package to run the Djerba application. Used by the command-line script `djerba.py`.
-
-See the [GitHub repo](https://github.com/oicr-gsi/djerba/blob/master/README.md) for details.
-"""
