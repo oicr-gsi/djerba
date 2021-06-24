4167839b1fd82e6961e9a9e51411ce0fa3a99fb2
iainrb <1695061+iainrb@users.noreply.github.com>
2021-06-24 14:05:57 -0400

    Fix for R markdown directory name

--- src/lib/djerba/render.py
+++ src/lib/djerba/render.py
@@ -9,10 +9,12 @@ import djerba.ini_fields as ini
 
 class html_renderer:
 
+    R_MARKDOWN_DIRNAME = 'R_markdown'
+
     def __init__(self, config):
         self.r_script_dir = config[ini.SETTINGS].get(ini.R_SCRIPT_DIR)
         if not self.r_script_dir:
-            self.r_script_dir = os.path.join(os.path.dirname(__file__), 'R')
+            self.r_script_dir = os.path.join(os.path.dirname(__file__), self.R_MARKDOWN_DIRNAME)
         self.markdown_script = os.path.join(self.r_script_dir, 'html_report.Rmd')
 
     def write_html(self, report_dir, out_path):
@@ -24,7 +26,7 @@ class html_renderer:
             report_dir
         ]
         print('###', ' '.join(cmd))
-        result = subprocess.run(cmd, check=True)
+        result = subprocess.run(cmd, check=True, capture_output=True)
         return result
 
 
