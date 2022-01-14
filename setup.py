ee30129172b8732834ef326b97fdb95a49a578ad
iainrb <1695061+iainrb@users.noreply.github.com>
2022-01-14 12:36:12 -0500

    Increment test version to 0.0.14b

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Setup script for Djerba
 
 from setuptools import setup, find_packages
 
-package_version = '0.0.14a'
+package_version = '0.0.14b'
 package_root = 'src/lib'
 
 with open("README.md", "r") as fh:
