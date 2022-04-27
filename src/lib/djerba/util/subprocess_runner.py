ad02fe37d5e771fa279ecf69b5c48705c2705e90
iainrb <1695061+iainrb@users.noreply.github.com>
2022-04-27 17:21:42 -0400

    First draft of subprocess_runner class, to replace existing calls to subprocess

---
+++ src/lib/djerba/util/subprocess_runner.py
@@ -0,0 +1,41 @@
+"""Base class with method to run a subprocess"""
+
+import subprocess
+from collections import Iterable
+from djerba.util.logger import logger
+import djerba.util.constants as constants
+
+class subprocess_runner(logger):
+
+    def __init__(self, log_level=logging.WARNING, log_path=None):
+        self.logger = self.get_logger(log_level, __name__, log_path)
+
+    def run_subprocess(self, command, description='subprocess', redact=[]):
+        if isinstance(command, str) or not isinstance(command, Iterable):
+            msg = "Command must be a non-string iterable: Received {0}".format(command)
+            self.logger.error(msg)
+            raise ValueError(msg)
+        if len(redact) > 0:
+            # redact selected arguments from logging, eg. passwords, access tokens
+            command_redacted = command.copy()
+            for i in range(len(command_redacted)):
+                if command_redacted[i] in redact:
+                    command_redacted[i+1] = '***REDACTED***'
+            logged_command = ''.join(command_redacted)
+        else:
+            logged_command = ''.join(command)
+        self.logger.info("Running {0}: '{1}'".format(description, logged_command))        
+        result = subprocess.run(command, capture_output=True)
+        stdout = result.stdout.decode(constants.TEXT_ENCODING)
+        stderr = result.stderr.decode(constants.TEXT_ENCODING)
+        try:
+            result.check_returncode()
+        except CalledProcessError as err:
+            self.logger.error("Failed to run {0}: {1}".format(description, err))
+            self.logger.error("{0} STDOUT: '{1}'".format(description, stdout))
+            self.logger.error("{0} STDERR: '{1}'".format(description, stderr))
+            raise
+        self.logger.info("Successfully ran {0}".format(description))
+        self.logger.debug("{0} STDOUT: '{1}'".format(description, stdout))
+        self.logger.debug("{0} STDERR: '{1}'".format(description, stderr))
+        return result
