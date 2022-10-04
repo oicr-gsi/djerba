c2cc7704b09a0c28a752169f6730aad4f200d7d5
wytong <wytong@192.168.0.116>
2022-10-04 13:32:53 -0400

    edited test.py for technical notes

--- src/test/test.py
+++ src/test/test.py
@@ -163,19 +163,19 @@ class TestConfigure(TestBase):
         test_configurer.run(out_path)
 
     def test_default(self):
-        self.run_config_test(self.config_user, False, False, 55, self.provenance)
+        self.run_config_test(self.config_user, False, False, 56, self.provenance)
 
     def test_default_fail(self):
-        self.run_config_test(self.config_user_failed, False, True, 45, self.provenance)
+        self.run_config_test(self.config_user_failed, False, True, 46, self.provenance)
 
     def test_wgs_only(self):
-        self.run_config_test(self.config_user_wgs_only, True, False, 53, self.provenance)
+        self.run_config_test(self.config_user_wgs_only, True, False, 54, self.provenance)
 
     def test_wgs_only_fail(self):
-        self.run_config_test(self.config_user_wgs_only_failed, True, True, 45, self.provenance)
+        self.run_config_test(self.config_user_wgs_only_failed, True, True, 46, self.provenance)
 
     def test_vnwgts(self):
-        self.run_config_test(self.config_user_vnwgts, False, False, 55, self.provenance_vnwgts)
+        self.run_config_test(self.config_user_vnwgts, False, False, 54, self.provenance_vnwgts)
 
     def test_vnwgts_broken(self):
         # test failure modes of sample input
