1e0759979f64f4bae3e5aedbf9ad601ca44932e2
Felix Beaudry <fbeaudry@oicr.on.ca>
2022-11-15 20:21:09 -0500

    transparent msi background

--- src/lib/djerba/R_plots/biomarkers_plot.R
+++ src/lib/djerba/R_plots/biomarkers_plot.R
@@ -19,7 +19,7 @@ work_dir <- opt$dir
 if(msi_b == "TRUE"){
   msi_path <- paste(work_dir, 'msi.txt', sep='/')
   
-  boot <- read.table(msi_path)
+  boot <- read.table(msi_path,header=FALSE)
   
   names(boot) <- c("q0","q1","median","q3","q4")
   boot$Sample <- "Sample"
@@ -27,21 +27,21 @@ if(msi_b == "TRUE"){
   msi_out_path <- paste(work_dir, 'msi.svg', sep='/')
   
   options(bitmapType='cairo')
-  svg(msi_out_path, width = 8, height = 1.5)
+  svg(msi_out_path, width = 8, height = 1.5,bg = "transparent")
   print(
     
   ggplot(boot,aes(x="Sample")) + 
-    geom_bar(aes(y=log(median)),fill='grey',stat ="identity",alpha=0.5,color="black") + 
-    geom_errorbar(aes(ymin=log(q1), ymax=log(q3)), width=0,size=2) +
-    geom_errorbar(aes(ymin=log(q0), ymax=log(q4)), width=0) +
+    geom_bar(aes(y=median),fill='grey',stat ="identity",alpha=0.5,color="black") + 
+    geom_errorbar(aes(ymin=q1, ymax=q3), width=0,size=2) +
+    geom_errorbar(aes(ymin=q0, ymax=q4), width=0) +
     
-    annotate(x = 0, xend=2, y=log(5), yend=log(5),geom="segment",linetype="longdash",colour = "red") +
-    annotate(geom="text",x = 0,y=log(5),color="black",label="MSI-H", hjust = -0.25, vjust = -6,size=3) +
+    annotate(x = 0, xend=2, y=5, yend=5,geom="segment",linetype="longdash",colour = "red") +
+    annotate(geom="text",x = 0,y=5,color="black",label="MSI-H", hjust = -0.25, vjust = -6,size=3) +
     
     guides(fill=FALSE)+
     theme_classic() + 
-    labs(x="",y="unstable microsatellites (%, log)",title="") + 
-    scale_y_continuous(expand = c(0,0), limit = c(0, log(100))) + 
+    labs(x="",y="unstable microsatellites (%)",title="") + 
+    scale_y_continuous(expand = c(0,0), limit = c(0, 100)) + 
     guides(alpha="none")+
     coord_flip() +
 
@@ -50,10 +50,12 @@ if(msi_b == "TRUE"){
           axis.title.y=element_blank(),
           axis.text.y=element_blank(),
           axis.ticks.y=element_blank(),
-          text = element_text(size = 10),
+          text = element_text(size = 18),
           panel.grid = element_blank(), 
-          plot.margin = unit(c(10, 10, 10, 10), "points"),
-          line = element_blank()
+          plot.margin = unit(c(0, 1, 0, 1), "points"),
+          line = element_blank(),
+          panel.background = element_rect(fill = "transparent", colour = NA),
+          plot.background = element_rect(fill="transparent",color=NA)
           ) 
   )
   dev.off()
