2da13f5c2ccc3453f4fe4c708242955fe9edd2a9
Lauren Toy <ltoy@oicr.on.ca>
2022-12-01 17:13:12 -0500

    brute onco combine

--- src/bin/db/extract_onco.R
+++ src/bin/db/extract_onco.R
@@ -3,11 +3,13 @@ library(ggplot2)
 library(data.table)
 library(forcats) # within tidyverse
 library(cowplot)
+library(gtable)
+library(grid)
 
 #ONCOPLOT ####
-small_file = '/home/ltoy/Desktop/couch/extract/small_Study&Body&Tumour Mutation Burden_processed.csv'
+small_file = '/home/ltoy/Desktop/couch/extract/oncoplot/small_Study&Body&Tumour Mutation Burden_processed.csv'
 small <- fread(small_file, header=TRUE)
-onco_file = '/home/ltoy/Desktop/couch/extract/onco_Study&Body&Tumour Mutation Burden_processed.csv'
+onco_file = '/home/ltoy/Desktop/couch/extract/oncoplot/onco_Study&Body&Tumour Mutation Burden_processed.csv'
 onco <- fread(onco_file, header=TRUE)
 small$Field <- 'small_mutations_and_indels'
 onco$Field <- 'oncogenic_somatic_CNVs'
@@ -69,7 +71,8 @@ o <-
                      sec.axis = (sec_axis(trans=~., breaks=seq(1.5, length(gene.key)+0.5,1),labels=rev(percent.key))))+
   labs(title = onco_title) + 
   theme(
-    plot.title = element_text(size=8, hjust=0.5),
+    plot.title=element_blank(), #blank if combine tmb and main below
+    #plot.title = element_text(size=8, hjust=0.5),
     axis.title.x=element_blank(), axis.title.y=element_blank(),
     axis.ticks.x=element_blank(),axis.ticks.y=element_blank(),
     axis.text.x=element_blank(),
@@ -88,11 +91,11 @@ o <-
                              "gold", "royalblue", "orange", "black", "firebrick1", "chartreuse"))+
   guides(fill = guide_legend(nrow = 2))#+
  #facet_grid(.~Study, scales="free_x", space="free_x")  #onco_x.png
-ggsave("/home/ltoy/Desktop/couch/extract/onco_percent.png", o, width=7,height=5)
+#ggsave("/home/ltoy/Desktop/couch/extract/onco_percent.png", o, width=7,height=5)
 #ggsave("/home/ltoy/Desktop/couch/extract/onco_x.png", o, width=6.5,height=5) 
 #ggsave("/home/ltoy/Desktop/couch/extract/onco_id.png", o, width=5,height=5) #id
 
-#oncobar_freq <- 
+oncobar_freq <- 
   ggplot(newdf)+
   geom_bar(aes(y=fct_rev(fct_infreq(Gene)), fill=Mutation))+
   labs(x="No. of Mutations")+
@@ -110,7 +113,7 @@ ggsave("/home/ltoy/Desktop/couch/extract/onco_percent.png", o, width=7,height=5)
   scale_fill_manual(values=c("saddlebrown","lightgoldenrod3", "forestgreen", "hotpink", "purple","cyan",
                                "gold", "royalblue", "orange", "black", "firebrick1", "chartreuse"))+
   scale_x_continuous(breaks=c(0,70), limits=c(0,70), expand=c(0,0), position="top")
-ggsave("/home/ltoy/Desktop/couch/extract/onco_bar.png", oncobar_freq, width=2.5,height=5) 
+#ggsave("/home/ltoy/Desktop/couch/extract/onco_bar.png", oncobar_freq, width=2.5,height=5) 
 
 id.key.df <- as.data.frame(levels(factor(newdf$id)))
 id.key.df$TMB <-NA
@@ -136,25 +139,28 @@ oncobar_tmb <-
   ggplot(id.key.df, aes(x=id, y=TMB, fill=Study))+
   geom_bar(position='dodge', stat='identity', aes(group=Study))+
   theme_classic()+
+  labs(title=onco_title)+
   theme(
-    legend.position = "top",
-   #legend.direction = "horizontal",
+    plot.title = element_text(size=10, hjust=0.5),
+    legend.position="none", # if combined separately below
+    #legend.position = "top",
     axis.text.x = element_blank(),
     #axis.text.x = element_text(size=5, angle=90), #id
-    axis.text.y=element_text(size=10),
-    axis.ticks.length.y = unit(4, "mm"),
-    axis.title.x=element_blank(), axis.title.y=element_text(size=15),
+    axis.text.y=element_text(size=7),
+    axis.ticks.length.y = unit(3, "mm"),
+    axis.title.x=element_blank(), axis.title.y=element_text(size=8),
     axis.line.x = element_blank(), axis.ticks.x = element_blank(),
-    legend.key.size = unit(8, "mm"),
+    legend.key.size = unit(5, "mm"),
     legend.title = element_blank(),
-    strip.text.x=element_blank()
+    strip.text.x=element_blank(),
+   aspect.ratio=(2.5/10)
   )+
-  guides(fill = guide_legend(nrow=1))+
+  guides(fill = guide_legend(ncol=3))+
   scale_fill_manual(values=c("PASS01"= "deepskyblue3", "CYPRESS"="indianred2", "HPB"="goldenrod", 
                              "PANXWGTS"="cyan3",  "LBR"="chartreuse4", "VENUS"="darkorchid2"))+
   scale_y_continuous(breaks=c(0,25,50,75,100),labels=c(0,25,50,75,'>100'), limits=c(0,100), expand=c(0,0))#+
  #facet_grid(.~Study, scales="free_x", space="free_x", switch="x")  #Study
-ggsave("/home/ltoy/Desktop/couch/extract/oncobar_tmb.png", oncobar_tmb, width=10,height=2.5) 
+#ggsave("/home/ltoy/Desktop/couch/extract/oncobar_tmb.png", oncobar_tmb, width=10,height=2.5) 
 
 ######################################################################################
 #extrac onco v small percents
@@ -169,13 +175,31 @@ ggplot(field_percent, aes(x="", y=Percent, fill=Var1))+
   coord_polar(theta="y")+
   geom_text(aes(label=paste(Var1,Percent, '%')), position=position_stack(vjust=0.5))+
   scale_fill_brewer("Blues")
+##########################################################################################
 
-#TO DO Formatting for combing 3 plots to make oncoplot in cowplot, currently align axes externally
-# comb<-
+top <- ggplotGrob(oncobar_tmb)
+main <- ggplotGrob(o)
+right <- ggplotGrob(oncobar_freq)
+vert_comb <- rbind(top, main, size="first")
+vert_comb$widths <- unit.pmax(top$widths, main$widths)
+grid.newpage()
+grid.draw(vert_comb)
+#ggsave("/home/ltoy/Desktop/couch/extract/vert_comb.png", vert_comb, width=10,height=10) 
+
+#to extract legend, must go back and uncomment legend.position = "none" to get element from oncobar_tmb then run below
+#however, for the code above in vert_comb, oncobar_tmb has no legend!
+tmb_legend <- get_legend(oncobar_tmb) #returns a gtable
+grid.newpage()
+grid.draw(tmb_legend)
+
+comb <- 
+  ggdraw()+ #empty canvas
+  draw_plot(right, x = 0.56, y=0.0377, width=0.21, height=0.793)+
+  draw_plot(vert_comb, x=-0.2, y=0.0) +
+  draw_plot(tmb_legend, x=0.17, y=0.4)
+ggsave("/home/ltoy/Desktop/couch/extract/comb.png", comb, width=15, height=10)
+
+###########################################################################################
 #   plot_grid(o, oncobar_freq, oncobar_tmb, nrow=1, scale=c(1,1,1)) 
-# ggsave("/home/ltoy/Desktop/couch/extract/comb.png", comb, width=10,height=5) 
-
-#geom_tile and geom_raster ####
-# ggplot(newdf) +
 #   geom_raster(aes(x=id, y=fct_rev(fct_infreq(Gene)), fill=Mutation), hjust=0, vjust=0)+
 #   #geom_tile(aes(x=id, y=fct_rev(fct_infreq(Gene)), fill=Mutation), color="black")+ theme()
