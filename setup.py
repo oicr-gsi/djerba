e04586c7ab63d4b6d0884f8189240c0a23d5eadd
Aqsa Alam <97138161+AqsaAlam@users.noreply.github.com>
2023-02-27 09:58:43 -0500

    Added data/versions.ini to find packages

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
             'data/technical_notes.txt',
             'data/hg38_centromeres.txt',
             'data/pgacomp-tcga.txt',
+            'data/versions.ini', # ADDED BY AQSA
             'html/clinical_report_template.html',
             'html/genomic_details_template.html',
             'html/header.html',
