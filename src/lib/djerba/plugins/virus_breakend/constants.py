6aa7825c9704492d4ad33f1693f18d44b6dd208e
Aqsa Alam <97138161+AqsaAlam@users.noreply.github.com>
2023-04-10 16:52:24 -0400

    Create constants.py

---
+++ src/lib/djerba/plugins/virus_breakend/constants.py
@@ -0,0 +1,15 @@
+"""
+This file contains a list of constants to be used in the VIRUSBreakend plugin.
+"""
+
+# Top-level variable names
+VIRUSBREAKEND = 'VIRUSBreakend'
+
+# Constants for the VIRUSBreakend table
+GENUS = 'Genus'
+SPECIES = 'Species'
+COVERAGE = 'Coverage'
+LENGTH = 'Length'
+MEANDEPTH = 'Mean depth'
+INTEGRATION = 'Integration'
+TOTAL_VARIANTS = 'Total variants'
