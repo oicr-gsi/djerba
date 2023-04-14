15bf248c6dad4470bd6003ae31ff5a8849fa4fe2
Iain Bancarz PhD <1695061+iainrb@users.noreply.github.com>
2023-04-14 17:18:12 -0400

    Working modification djerba.py script to generate a report in core.main; other modes TODO

--- src/lib/djerba/core/main.py
+++ src/lib/djerba/core/main.py
@@ -5,9 +5,11 @@ Main class to:
 - Merge and output results
 """
 from configparser import ConfigParser
-import logging
 import json
+import logging
+import os
 import djerba.util.ini_fields as ini
+import djerba.version as version
 from djerba.core.configure import configurer as core_configurer
 from djerba.core.extract import extractor as core_extractor
 from djerba.core.json_validator import plugin_json_validator
@@ -16,6 +18,7 @@ from djerba.core.loaders import plugin_loader, merger_loader
 from djerba.core.workspace import workspace
 from djerba.util.logger import logger
 from djerba.util.validator import path_validator
+import djerba.util.constants as constants
 
 class main(logger):
 
@@ -25,13 +28,18 @@ class main(logger):
     MERGERS = 'mergers'
     MERGE_INPUTS = 'merge_inputs'
     
-    def __init__(self, work_dir, log_level=logging.INFO, log_path=None):
+    def __init__(self, work_dir=None, log_level=logging.INFO, log_path=None):
         self.log_level = log_level
         self.log_path = log_path
         self.logger = self.get_logger(log_level, __name__, log_path)
+        self.logger.info("Running Djerba version {0}".format(version.__version__))
         self.json_validator = plugin_json_validator(self.log_level, self.log_path)
         self.path_validator = path_validator(self.log_level, self.log_path)
-        self.workspace = workspace(work_dir, self.log_level, self.log_path)
+        self.work_dir = work_dir
+        if self.work_dir != None:
+            self.workspace = workspace(work_dir, self.log_level, self.log_path)
+        else:
+            self.workspace = None # eg. no workspace needed for 'render' only
         self.plugin_loader = plugin_loader(self.log_level, self.log_path)
         self.merger_loader = merger_loader(self.log_level, self.log_path)
 
@@ -69,6 +77,7 @@ class main(logger):
         return merger.render(merger_inputs)
 
     def configure(self, config_path_in, config_path_out=None):
+        self.logger.info('Starting Djerba config step')
         if config_path_out:  # do this *before* taking the time to generate output
             self.validator.validate_output_file(config_path_out)
         config_in = self.read_ini_path(config_path_in)
@@ -86,9 +95,11 @@ class main(logger):
         if config_path_out:
             with open(config_path_out, 'w') as out_file:
                 config_out.write(out_file)
+        self.logger.info('Finished Djerba config step')
         return config_out
 
     def extract(self, config, json_path=None):
+        self.logger.info('Starting Djerba extract step')
         if json_path:  # do this *before* taking the time to generate output
             self.validator.validate_output_file(json_path)
         data = core_extractor(self.log_level, self.log_path).run(config)
@@ -103,9 +114,11 @@ class main(logger):
         if json_path:
             with open(json_path, 'w') as out_file:
                 out_file.write(json.dumps(data))
+        self.logger.info('Finished Djerba extract step')
         return data
 
     def render(self, data, html_path=None):
+        self.logger.info('Starting Djerba render step')
         if html_path:  # do this *before* taking the time to generate output
             self.path_validator.validate_output_file(html_path)
         [header, footer] = core_renderer(self.log_level, self.log_path).run(data)
@@ -132,6 +145,8 @@ class main(logger):
         if html_path:
             with open(html_path, 'w') as out_file:
                 out_file.write(html)
+            self.logger.info("Wrote HTML output to {0}".format(html_path))
+        self.logger.info('Finished Djerba render step')
         return html
 
     def read_ini_path(self, ini_path):
@@ -140,12 +155,150 @@ class main(logger):
         config.read(ini_path)
         return config
 
-    # TODO write a run() method to:
-    # - convert command-line args to appropriate inputs
-    # - run configure/extract/render as required
-    # - Do PDF conversion
     def run(self, args):
-        pass
+        # run from command-line args
+        # path validation was done in command-line script
+        ap = arg_processor(args, validate=False)
+        mode = ap.get_mode()
+        if mode == constants.REPORT:
+            # get operational parameters
+            work_dir = ap.get_work_dir()
+            ini_path = ap.get_ini_path()
+            ini_path_out = ap.get_ini_out_path()
+            json_path = ap.get_json_path()
+            html_path = ap.get_html_path()
+            #pdf_path = ap.get_pdf()
+            # TODO define archive/cleanup switches for extract
+            # TODO apply/update cache switch for extract
+            #archive = ap.is_archive_enabled()
+            #cleanup = ap.is_cleanup_enabled()
+            config = self.configure(ini_path, ini_path_out)
+            data = self.extract(config, json_path)
+            self.render(data, html_path)
+            # TODO if pdf_path!=None, convert HTML->PDF
+        else:
+            # TODO add clauses for setup, configure, etc.
+            # for now, raise an error
+            msg = "Mode '{0}' is not defined in Djerba core.main!".format(mode)
+            self.logger.error(msg)
+            raise RuntimeError(msg)
+
+class arg_processor(logger):
+    # class to process command-line args for creating a main object
+
+    def __init__(self, args, validate=True):
+        self.args = args
+        self.log_level = self.get_args_log_level(self.args)
+        self.log_path = self.args.log_path
+        if self.log_path:
+            # we are verifying the log path, so don't write output there yet
+            path_validator(self.log_level).validate_output_file(self.log_path)
+        self.logger = self.get_logger(self.log_level, __name__, self.log_path)
+        if validate:
+            self.validate_args(self.args)  # checks subparser and args are valid
+        self.mode = self.args.subparser_name
+
+    def _get_arg(self, arg_name):
+        try:
+            value = getattr(self.args, arg_name)
+        except AttributeError as err:
+            msg = "Argument {0} not defined in Djerba mode {1}".format(arg_name, self.mode)
+            self.logger.error(msg)
+            raise ArgumentNameError(msg) from err
+        return value
+
+    def get_ini_path(self):
+        return self._get_arg('ini')
+
+    def get_ini_out_path(self):
+        return self._get_arg('ini_out')
+
+    def get_json_path(self):
+        return self._get_arg('json')
+    
+    def get_log_level(self):
+        return self.log_level
+
+    def get_log_path(self):
+        return self.log_path
+
+    def get_html_path(self):
+        if hasattr(self.args, 'html') and self._get_arg('html') != None:
+           value = self._get_arg('html')
+        else:
+            work_dir = self.get_work_dir()
+            if work_dir == None:
+                msg = "Cannot find default HTML path, work_dir undefined"
+                self.logger.error(msg)
+                raise RuntimeError(msg)
+            value = os.path.join(work_dir, 'djerba_report.html')
+        return value
+
+    def get_mode(self):
+        return self.mode
+
+    def get_pdf_path(self):
+        return self._get_arg('pdf')
+    
+    def get_work_dir(self):
+        # default to None if work_dir is not in args
+        # appropriate if eg. only running the 'render' step
+        if hasattr(self.args, 'work_dir'):
+           value = self._get_arg('work_dir')
+        else:
+            value = None
+        return value
+
+    def is_archive_enabled(self):
+        return not self._get_arg('no_archive')
+
+    def is_cleanup_enabled(self):
+        return not self._get_arg('no_cleanup')
+    
+    def validate_args(self, args):
+        """
+        Check we can read/write paths in command-line arguments
+        Assume logging has been initialized and log path (if any) is valid
+        """
+        self.logger.info("Validating paths in command-line arguments")
+        v = path_validator(self.log_level, self.log_path)
+        if args.subparser_name == constants.SETUP:
+            v.validate_output_dir(args.base)
+        elif args.subparser_name == constants.CONFIGURE:
+            v.validate_input_file(args.ini)
+            v.validate_output_file(args.out)
+            v.validate_output_dir(args.work_dir)
+        elif args.subparser_name == constants.EXTRACT:
+            v.validate_input_file(args.ini)
+            v.validate_output_dir(args.work_dir)
+            if args.json:
+                v.validate_output_file(args.json)
+        elif args.subparser_name == constants.HTML:
+            v.validate_input_file(args.json)
+            v.validate_output_file(args.html)
+            if args.pdf:
+                v.validate_output_file(args.pdf)
+        elif args.subparser_name == constants.PDF:
+            v.validate_input_file(args.json)
+            v.validate_output_dir(args.pdf)
+        elif args.subparser_name == constants.REPORT:
+            v.validate_input_file(args.ini)
+            v.validate_output_dir(args.work_dir)
+            if args.ini_out:
+                v.validate_output_file(args.ini_out)
+            if args.html:
+                v.validate_output_file(args.pdf)
+            if args.json:
+                v.validate_output_file(args.json)
+            if args.pdf:
+                v.validate_output_file(args.pdf)
+        else:
+            # shouldn't happen, but handle this case for completeness
+            raise ValueError("Unknown subparser: " + args.subparser_name)
+        self.logger.info("Command-line path validation finished.")
+
+class ArgumentNameError(Exception):
+    pass
 
 class ComponentNameError(Exception):
     pass
