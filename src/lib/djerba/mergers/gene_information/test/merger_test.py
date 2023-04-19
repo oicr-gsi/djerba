ccfc331834083a0282c8378306197e1a4ebb30b3
Iain Bancarz PhD <1695061+iainrb@users.noreply.github.com>
2023-04-19 11:44:24 -0400

    Move test of the gene information merger from core to the merger directory

---
+++ src/lib/djerba/mergers/gene_information/test/merger_test.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+"""Test of the gene information merger"""
+
+import json
+import logging
+import os
+import unittest
+from configparser import ConfigParser
+from djerba.core.loaders import helper_loader
+from djerba.core.workspace import workspace
+from djerba.util.testing.tools import TestBase
+from djerba.mergers.gene_information.merger import main as gene_information_merger_main
+
+class TestGeneInformationMerger(TestBase):
+
+    GENE_INFO_INPUTS = 'gene_information_inputs.json'
+
+    def test_gene_info(self):
+        test_source_dir = os.path.realpath(os.path.dirname(__file__))
+        json_path = os.path.join(test_source_dir, self.GENE_INFO_INPUTS)
+        with open(json_path) as json_file:
+            inputs = json.loads(json_file.read())
+        html = gene_information_merger_main().render(inputs)
+        md5_found = self.getMD5_of_string(html)
+        self.assertEqual(md5_found, 'd436df8d05a8af3cbdf71a15eb12f7ea')
+
+if __name__ == '__main__':
+    unittest.main()
