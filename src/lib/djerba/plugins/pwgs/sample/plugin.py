3469f279dc88d428c7a036e3d635df685d83127f
Felix Beaudry <Felix.Beaudry@oicr.on.ca>
2023-04-21 10:50:03 -0400

    pull from qc-etl

--- src/lib/djerba/plugins/pwgs/sample/plugin.py
+++ src/lib/djerba/plugins/pwgs/sample/plugin.py
@@ -10,13 +10,21 @@ import djerba.plugins.pwgs.constants as constants
 from djerba.util.subprocess_runner import subprocess_runner
 from djerba.util.logger import logger
 
+try:
+    import gsiqcetl.column
+    from gsiqcetl import QCETLCache
+except ImportError:
+        raise ImportError('Error Importing QC-ETL, try checking python versions')
+
 class main(plugin_base):
 
     def configure(self, config_section):
         return config_section
 
     def extract(self, config_section):
-        self.logger.info("PWGS: No extracting necessary")       
+        self.logger.info("PWGS: No extracting necessary")
+        tumour_id = self.workspace.read_core_config()['tumour_id']
+        qc_dict = self.fetch_coverage_etl_data(tumour_id)
         data = {
             'plugin_name': 'pwgs.sample',
             'clinical': True,
@@ -25,9 +33,9 @@ class main(plugin_base):
                 'gene_information': []
             },
             'results': {
-                'median_insert_size': config_section[constants.INSERT_SIZE],
+                'median_insert_size': qc_dict['insertSize'],
                 'tumour_fraction': config_section[constants.TUMOUR_FRACTION_READS],
-                'coverage': config_section[constants.COVERAGE],
+                'coverage': qc_dict['coverage'],
                 'primary_snv_count': config_section[constants.SNV_COUNT]
             }
         }
@@ -49,3 +57,24 @@ class main(plugin_base):
             self.logger.error(msg)
             raise
         return html    
+    
+    def fetch_coverage_etl_data(self,tumour_id):
+        self.qcetl_cache = "/scratch2/groups/gsi/production/qcetl_v1"
+        self.etl_cache = QCETLCache(self.qcetl_cache)
+        cached_coverages = self.etl_cache.bamqc4merged.bamqc4merged
+        columns_of_interest = gsiqcetl.column.BamQc4MergedColumn
+        data = cached_coverages.loc[
+            (cached_coverages[columns_of_interest.GroupID] == tumour_id),
+            [columns_of_interest.GroupID, columns_of_interest.CoverageDeduplicated, columns_of_interest.InsertMedian]
+            ]
+        if len(data) > 0:
+           qc_dict = {'coverage' : round(data.iloc[0][columns_of_interest.CoverageDeduplicated].item(),1)}
+           qc_dict['insertSize'] = round(data.iloc[0][columns_of_interest.InsertMedian].item(),1)
+           return(qc_dict)
+        else:
+            msg = "Djerba couldn't find the QC metrics associated with tumour_id {0} in QC-ETL. ".format(tumour_id)
+            self.logger.debug(msg)
+            raise MissingQCETLError(msg)
+    
+class MissingQCETLError(Exception):
+    pass 
--- src/lib/djerba/plugins/pwgs/test/test_pwgs.py
+++ src/lib/djerba/plugins/pwgs/test/test_pwgs.py
@@ -13,22 +13,22 @@ class TestPwgsPlugins(PluginTester):
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
         params = {
             self.INI: 'data/pwgs.analysis.ini',
-            self.JSON: 'data/report/pwgs.analysis.json',
-            self.MD5: '28cc6bd2bbc66103a4bf0f6656a989e7'
+            self.JSON: '/.mounts/labs/CGI/scratch/fbeaudry/reporting/djerba_test_data_lfs/pwgs-plugin/report_json/pwgs.analysis.json',
+            self.MD5: 'd1f56cc16792eddfdde23be08c7e2518'
         }
         self.run_basic_test(test_source_dir, params)
 
     def testPreprocessHbc(self):
-        hbc_result = analysis.main.preprocess_hbc(self,'/.mounts/labs/CGI/scratch/fbeaudry/reporting/djerba/src/lib/djerba/plugins/pwgs/test/data/report/HBCs.csv')
+        hbc_result = analysis.main.preprocess_hbc(self,'/.mounts/labs/CGI/scratch/fbeaudry/reporting/djerba_test_data_lfs/pwgs-plugin/HBCs.csv')
         self.assertEqual(hbc_result['sites_checked'], 123616)
         self.assertEqual(hbc_result['hbc_n'], 24)
 
     def testPreprocessvaf(self):
-        reads_detected = analysis.main.preprocess_vaf(self,'/.mounts/labs/CGI/scratch/fbeaudry/reporting/djerba/src/lib/djerba/plugins/pwgs/test/data/report/mrdetect.vaf.txt')
+        reads_detected = analysis.main.preprocess_vaf(self,'/.mounts/labs/CGI/scratch/fbeaudry/reporting/djerba_test_data_lfs/pwgs-plugin/mrdetect.vaf.txt')
         self.assertEqual(reads_detected, 9241)
 
     def testPreprocessResults(self):
-        results = analysis.main.preprocess_results(self,'/.mounts/labs/CGI/scratch/fbeaudry/reporting/djerba/src/lib/djerba/plugins/pwgs/test/data/report/mrdetect.txt')
+        results = analysis.main.preprocess_results(self,'/.mounts/labs/CGI/scratch/fbeaudry/reporting/djerba_test_data_lfs/pwgs-plugin/mrdetect.txt')
         self.assertEqual(results['TF'], 0.0321 )
         self.assertEqual(results['pvalue'], 1.903e-05)
         self.assertEqual(results['outcome'], 'POSITIVE')
