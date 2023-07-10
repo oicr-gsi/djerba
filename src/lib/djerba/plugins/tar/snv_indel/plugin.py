db9a06e4f9fa74e9e79f9f81d907c9e5969cfe31
Aqsa Alam <97138161+AqsaAlam@users.noreply.github.com>
2023-07-10 14:22:13 -0400

    Update plugin.py

--- src/lib/djerba/plugins/tar/snv_indel/plugin.py
+++ src/lib/djerba/plugins/tar/snv_indel/plugin.py
@@ -1,1 +1,69 @@
+"""
+Plugin for TAR SNV Indel
+"""
 
+# IMPORTS
+import os
+from djerba.plugins.base import plugin_base
+from mako.lookup import TemplateLookup
+import djerba.plugins.snv_indel.constants as constants
+from djerba.plugins.snv_indel.preprocess import preprocess
+from djerba.plugins.snv_indel.extract import data_builder 
+import djerba.core.constants as core_constants
+from djerba.util.subprocess_runner import subprocess_runner
+import djerba.util.provenance_index as index
+from djerba.core.workspace import workspace
+
+class main(plugin_base):
+   
+    PRIORITY = 100
+    PLUGIN_VERSION = '1.0.0'
+    TEMPLATE_NAME = 'snv_indel_template.html'
+    
+    def __init__(self, **kwargs):
+      super().__init__(**kwargs)
+         
+    def specify_params(self):
+      self.set_ini_default(core_constants.CLINICAL, True)
+      self.set_ini_default(core_constants.SUPPLEMENTARY, False)
+      self.set_priority_defaults(self.PRIORITY)
+
+    def configure(self, config):
+      config = self.apply_defaults(config)
+      return config  
+
+    def extract(self, config):
+      
+      wrapper = self.get_config_wrapper(config)  
+      # Pre-process all the files
+      # self.preprocess()
+      work_dir = self.workspace.get_work_dir()
+      work_dir = "."
+      #print(work_dir)
+      #preprocess(work_dir).run_R_code()
+
+      data = {
+          'plugin_name': 'Tar SNV Indel',
+          'version': self.PLUGIN_VERSION,
+          'priorities': wrapper.get_my_priorities(),
+          'attributes': wrapper.get_my_attributes(),
+          'merge_inputs': {},
+          'results': data_builder(work_dir).build_small_mutations_and_indels()
+      }
+      return data
+
+    def render(self, data):
+      args = data
+      html_dir = os.path.realpath(os.path.join(
+          os.path.dirname(__file__),
+          'html'
+      ))
+      report_lookup = TemplateLookup(directories=[html_dir, ], strict_undefined=True)
+      mako_template = report_lookup.get_template(self.TEMPLATE_NAME)
+      try:
+          html = mako_template.render(**args)
+      except Exception as err:
+          msg = "Unexpected error of type {0} in Mako template rendering: {1}".format(type(err).__name__, err)
+          self.logger.error(msg)
+          raise
+      return html
