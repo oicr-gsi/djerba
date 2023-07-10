266e716cd90dbcf659e1293293e6840a9d760300
Aqsa Alam <97138161+AqsaAlam@users.noreply.github.com>
2023-07-10 14:34:24 -0400

    Create constants.py

---
+++ src/lib/djerba/plugins/tar/sample/constants.py
@@ -0,0 +1,27 @@
+# AUTHOR: Felix Beaudry 
+
+# sample variable
+ONCOTREE = 'oncotree'
+KNOWN_VARIANTS = 'known_variants'
+CANCER_CONTENT = 'cancer_content'
+RAW_COVERAGE = 'raw_coverage'
+UNIQUE_COVERAGE = 'unique_coverage'
+
+# sample constants
+ASSAY = 'assay'
+AUTHOR = 'author'
+DONOR = 'root_sample_name'
+GROUP_ID = 'group_id'
+PATIENT_ID = 'Patient Study ID'
+PRIMARY_CANCER = 'primary_cancer'
+PWGS_REPORT = 'pwgs_report_id'
+REQ_APPROVED = 'requisition_approved'
+STUDY = 'study_title'
+WGS_REPORT = 'wgs_report_id'
+
+# file constants
+BAMQC = 'bamQC_results'
+PROVENANCE_OUTPUT = 'provenance_subset.tsv.gz'
+
+# render constants
+SAMPLE_TEMPLATE_NAME = 'sample_template.html'
