f841f7bd2f27c2aa0674cf639ea5e7dd49f839f1
Aqsa Alam <97138161+AqsaAlam@users.noreply.github.com>
2023-07-10 15:32:38 -0400

    Create constants.py

---
+++ src/lib/djerba/plugins/tar/swgs/constants.py
@@ -0,0 +1,18 @@
+"""
+This file contains a list of constants to be used in the sWGS plugin.
+AUTHOR: Aqsa Alam
+"""
+
+# For rendering
+TOTAL_VARIANTS = 'Total variants'
+CLINICALLY_RELEVANT_VARIANTS = 'Clinically relevant variants'
+HAS_EXPRESSION_DATA = 'Has expression data'
+BODY = 'Body'
+GENE = 'Gene'
+GENE_URL = 'Gene_URL'
+CHROMOSOME = 'Chromosome'
+ALTERATION = 'Alteration'
+ONCOKB = 'OncoKB'
+EXPRESSION_METRIC = 'Expression Percentile'
+CNV_PLOT = 'cnv_plot'
+TEXT_ENCODING = 'utf-8'
