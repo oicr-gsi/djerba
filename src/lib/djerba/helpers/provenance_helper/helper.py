7d1944aaae44546952b6879e2f0fd40595dea879
Iain Bancarz PhD <1695061+iainrb@users.noreply.github.com>
2023-07-20 17:43:40 -0400

    Run provenance reader and write sample info in config step

--- src/lib/djerba/helpers/provenance_helper/helper.py
+++ src/lib/djerba/helpers/provenance_helper/helper.py
@@ -3,7 +3,7 @@
 import csv
 import gzip
 import logging
-import djerba.util.ini_fields as ini
+import djerba.core.constants as core_constants
 import djerba.util.provenance_index as index
 from djerba.helpers.base import helper_base
 from djerba.util.provenance_reader import provenance_reader, sample_name_container, \
@@ -11,24 +11,80 @@ from djerba.util.provenance_reader import provenance_reader, sample_name_contain
 
 class main(helper_base):
 
-    PROVENANCE_INPUT = 'provenance_input_path'
+    DEFAULT_PROVENANCE_INPUT = '/scratch2/groups/gsi/production/vidarr/'+\
+        'vidarr_files_report_latest.tsv.gz'
+    PROVENANCE_INPUT_KEY = 'provenance_input_path'
     STUDY_TITLE = 'study_title'
     ROOT_SAMPLE_NAME = 'root_sample_name'
     PROVENANCE_OUTPUT = 'provenance_subset.tsv.gz'
     PRIORITY = 50
 
-    # No automated configuration; use placeholder method of parent class
-    # Needs study title, root sample name, provenance path; configured manually (for now)
-    # Optionally, provide WG-N/WG-T/WT-T sample names in INI
-
+    # constants previously in ini_fields.py
+    NORMAL_ID = 'normalid'
+    TUMOUR_ID = 'tumourid'
+    PATIENT_ID = 'patientid'
+    SAMPLE_NAME_WG_N = 'sample_name_whole_genome_normal' # whole genome, normal
+    SAMPLE_NAME_WG_T = 'sample_name_whole_genome_tumour' # whole genome, tumour
+    SAMPLE_NAME_WT_T = 'sample_name_whole_transcriptome' # whole transcriptome, tumour
+    
+    def configure(self, config):
+        config = self.apply_defaults(config)
+        wrapper = self.get_config_wrapper(config)
+        provenance_path = wrapper.get_my_string(self.PROVENANCE_INPUT_KEY)
+        study = wrapper.get_my_string(self.STUDY_TITLE)
+        donor = wrapper.get_my_string(self.ROOT_SAMPLE_NAME)
+        self.write_provenance_subset(study, donor, provenance_path)      
+        self.logger.debug("configure: Wrote provenance subset to workspace")
+        # parse the file provenance subset and write sample_info.json to the workspace
+        samples = sample_name_container()
+        if wrapper.my_param_is_not_null(self.SAMPLE_NAME_WG_N):
+            samples.set_wg_n(wrapper.get_my_string(self.SAMPLE_NAME_WG_N))
+        if wrapper.my_param_is_not_null(self.SAMPLE_NAME_WG_T):
+            samples.set_wg_t(wrapper.get_my_string(self.SAMPLE_NAME_WG_T))
+        if wrapper.my_param_is_not_null(self.SAMPLE_NAME_WT_T):
+            samples.set_wt_t(wrapper.get_my_string(self.SAMPLE_NAME_WT_T))
+        if samples.is_valid():
+            self.logger.debug("Sample names from INI are valid: {0}".format(samples))
+        else:
+            msg = "Invalid sample name configuration: {0}.".format(samples)
+            msg = msg + " Must either be empty, or have at least WG tumour/normal names"
+            self.logger.error(msg)
+            raise InvalidConfigurationError(msg)
+        # get canonical names from the file provenance reader
+        names = self.write_sample_info(study, donor, samples)
+        self.logger.debug("configure: Wrote sample info to workspace")
+        for key in names.keys():
+            wrapper.set_my_param(key, names.get(key))
+        return wrapper.get_config()
+    
     def extract(self, config):
         self.validate_full_config(config)
         wrapper = self.get_config_wrapper(config)
-        provenance_path = wrapper.get_my_string(self.PROVENANCE_INPUT)
+        provenance_path = wrapper.get_my_string(self.PROVENANCE_INPUT_KEY)
         study = wrapper.get_my_string(self.STUDY_TITLE)
         donor = wrapper.get_my_string(self.ROOT_SAMPLE_NAME)
+        if self.workspace.has_file(self.PROVENANCE_OUTPUT):
+            msg = "extract: {0} ".format(self.PROVENANCE_OUTPUT)+\
+                "already in workspace, will not overwrite"
+            self.logger.debug(msg)
+        else:
+            self.write_provenance_subset(study, donor, provenance_path)
+            self.logger.debug("extract: Wrote provenance subset to workspace")
+
+    def specify_params(self):
+        self.logger.debug("Specifying params for provenance helper")
+        self.set_ini_default(self.PROVENANCE_INPUT_KEY, self.DEFAULT_PROVENANCE_INPUT)
+        self.add_ini_required(self.STUDY_TITLE)
+        self.add_ini_required(self.ROOT_SAMPLE_NAME)
+        self.set_priority_defaults(self.PRIORITY)
+        self.set_ini_null_default(self.SAMPLE_NAME_WG_N)
+        self.set_ini_null_default(self.SAMPLE_NAME_WG_T)
+        self.set_ini_null_default(self.SAMPLE_NAME_WT_T)
+
+    def write_provenance_subset(self, study, donor, provenance_path):
         self.logger.info('Started reading file provenance from {0}'.format(provenance_path))
         total = 0
+        kept = 0
         with gzip.open(provenance_path, 'rt') as in_file, \
              self.workspace.open_gzip_file(self.PROVENANCE_OUTPUT, write=True) as out_file:
             reader = csv.reader(in_file, delimiter="\t")
@@ -36,31 +92,28 @@ class main(helper_base):
             for row in reader:
                 total += 1
                 if total % 100000 == 0:
-                    self.logger.debug("Read {0} rows".format(total))
+                    self.logger.debug("Read {0} input rows".format(total))
                 if row[index.STUDY_TITLE] == study and row[index.ROOT_SAMPLE_NAME] == donor:
                     writer.writerow(row)
-        self.logger.debug('Done; read {0} rows'.format(total))
+                    kept += 1
+        self.logger.debug('Done reading FPR; kept {0} of {1} rows'.format(kept, total))
         self.logger.info('Finished reading file provenance from {0}'.format(provenance_path))
-        # parse the file provenance subset and write sample_info.json to the workspace
-        samples = sample_name_container()
-        if wrapper.has_my_param(ini.SAMPLE_NAME_WG_N):
-            samples.set_wg_n(wrapper.get_my_param(key))
-        if wrapper.has_my_param(ini.SAMPLE_NAME_WG_T):
-            samples.set_wg_t(wrapper.get_my_param(key))
-        if wrapper.has_my_param(ini.SAMPLE_NAME_WT_T):
-            samples.set_wt_t(wrapper.get_my_param(key))
-        if samples.is_valid():
-            self.logger.debug("Configured sample names are valid")
-        else:
-            msg = "Invalid sample name configuration: {0}.".format(samples)
-            msg = msg + " Must either be empty, or have at least WG tumour/normal names"
-            self.logger.error(msg)
-            raise InvalidConfigurationError(msg)
+
+    def write_sample_info(self, study, donor, samples):
         subset_path = self.workspace.abs_path(self.PROVENANCE_OUTPUT)
         reader = provenance_reader(subset_path, study, donor, samples)
         names = reader.get_sample_names()
-
-    def specify_params(self):
-        self.logger.debug("Specifying params for provenance helper")
-        self.add_ini_required(self.PROVENANCE_INPUT)
-        self.set_priority_defaults(self.PRIORITY)
+        ids = reader.get_identifiers()
+        # TODO should the tumour/normal IDs be INI parameters?
+        sample_info = {
+            self.STUDY_TITLE: study,
+            self.ROOT_SAMPLE_NAME: donor,
+            core_constants.TUMOUR_ID: ids.get(self.TUMOUR_ID),
+            core_constants.NORMAL_ID: ids.get(self.NORMAL_ID),
+            self.SAMPLE_NAME_WG_T: names.get(self.SAMPLE_NAME_WG_T),
+            self.SAMPLE_NAME_WG_N: names.get(self.SAMPLE_NAME_WG_N),
+            self.SAMPLE_NAME_WT_T: names.get(self.SAMPLE_NAME_WT_T)
+        }
+        self.workspace.write_json(core_constants.DEFAULT_SAMPLE_INFO, sample_info)
+        self.logger.debug("Wrote sample info to workspace: {0}".format(sample_info))
+        return names
--- src/lib/djerba/helpers/provenance_helper/test/helper_test.py
+++ src/lib/djerba/helpers/provenance_helper/test/helper_test.py
@@ -6,6 +6,7 @@ import logging
 import os
 import unittest
 from configparser import ConfigParser
+import djerba.core.constants as core_constants
 from djerba.core.loaders import helper_loader
 from djerba.core.workspace import workspace
 from djerba.util.testing.tools import TestBase
@@ -19,24 +20,30 @@ class TestProvenanceHelper(TestBase):
     def test(self):
         data_dir = os.path.join(os.environ.get('DJERBA_TEST_DATA'), 'helpers', 'provenance')
         provenance_input = os.path.join(data_dir, 'provenance_input.tsv.gz')
-        cp = ConfigParser()
-        cp.add_section(self.CORE)
-        cp.set(self.CORE, 'study_title', 'PASS01')
-        cp.set(self.CORE, 'root_sample_name', 'PANX_1500')
-        cp.add_section(self.HELPER_NAME)
-        cp.set(self.HELPER_NAME, 'provenance_input_path', provenance_input)
-        ini_path = os.path.join(self.tmp_dir, 'test.ini')
-        with open(ini_path, 'w') as ini_file:
-            cp.write(ini_file)
         ws = workspace(self.tmp_dir)
         loader = helper_loader(logging.WARNING)
         helper_main = loader.load(self.HELPER_NAME, ws)
-        config = helper_main.configure(cp)
-        helper_main.extract(config)
+        config = helper_main.get_expected_config()
+        config.add_section(self.CORE)
+        config.set(self.HELPER_NAME, 'study_title', 'PASS01')
+        config.set(self.HELPER_NAME, 'root_sample_name', 'PANX_1500')
+        config.set(self.HELPER_NAME, 'provenance_input_path', provenance_input)
+        config = helper_main.configure(config)
         out_path = os.path.join(self.tmp_dir, helper_main.PROVENANCE_OUTPUT)
         self.assertTrue(os.path.exists(out_path))
         self.assertEqual(self.getMD5_of_gzip_path(out_path),
                          '62c0b00c42a352d9ce3c49aedb55e8e2')
+        sample_info_path = os.path.join(self.tmp_dir, core_constants.DEFAULT_SAMPLE_INFO)
+        self.assertTrue(os.path.exists(sample_info_path))
+        self.assertEqual(self.getMD5(sample_info_path), '406227ea4d73df43d0e4adcd02fc14cb')
+        # TODO suppress console output from helper_main_2
+        loader_2 = helper_loader(log_level=logging.DEBUG)
+        helper_main_2 = loader_2.load(self.HELPER_NAME, ws)
+        with self.assertLogs(level=logging.DEBUG) as log_context:
+            helper_main_2.extract(config)
+        msg = 'DEBUG:djerba.core.configure:extract: provenance_subset.tsv.gz already '+\
+            'in workspace, will not overwrite'
+        self.assertIn(msg, log_context.output)
 
 if __name__ == '__main__':
     unittest.main()
