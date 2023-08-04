9c3bb26ee82cc33d7ab17d6cc4b3fe2b379157ea
Felix Beaudry <Felix.Beaudry@oicr.on.ca>
2023-08-04 15:17:31 -0400

    updated testing

--- src/lib/djerba/plugins/supplement/test/test_supplement.py
+++ src/lib/djerba/plugins/supplement/test/test_supplement.py
@@ -10,34 +10,41 @@ from djerba.util.validator import path_validator
 from djerba.plugins.plugin_tester import PluginTester
 
 class TestPwgSupplementaryPlugin(PluginTester):
+    def setUp(self):
+        self.path_validator = path_validator()
+        self.maxDiff = None
+        self.tmp = tempfile.TemporaryDirectory(prefix='djerba_')
+        self.tmp_dir = self.tmp.name
+        sup_dir_var = 'DJERBA_TEST_DATA'
+        self.sup_dir = os.environ.get(sup_dir_var)
 
     def testPwgsSupplementary(self):
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
-        json_location = os.path.join(self.sup_dir ,"pwgs-plugin/report_json/pwgs.supp.json")
+        json_location = os.path.join(self.sup_dir ,"supplement-plugin/report_json/pwgs.supplement.json")
         params = {
             self.INI: 'PWGS.supp.ini',
             self.JSON: json_location,
-            self.MD5: '7abc0caa45be2b1fe8b4fe21bf41c91b'
+            self.MD5: '4ad37563321849bb8de56575032cec00'
         }
         self.run_basic_test(test_source_dir, params)
 
     def testTarSupplementary(self):
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
-        json_location = os.path.join(self.sup_dir ,"pwgs-plugin/report_json/pwgs.supp.json")
+        json_location = os.path.join(self.sup_dir ,"supplement-plugin/report_json/tar.supplement.json")
         params = {
             self.INI: 'TAR.supp.ini',
             self.JSON: json_location,
-            self.MD5: '7abc0caa45be2b1fe8b4fe21bf41c91b'
+            self.MD5: '4ad79b0cfa5b7062999c4392fa1a6681'
         }
         self.run_basic_test(test_source_dir, params)
 
     def testWgtsSupplementary(self):
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
-        json_location = os.path.join(self.sup_dir ,"pwgs-plugin/report_json/pwgs.supp.json")
+        json_location = os.path.join(self.sup_dir ,"supplement-plugin/report_json/wgts.supplement.json")
         params = {
             self.INI: 'WGTS.supp.ini',
             self.JSON: json_location,
-            self.MD5: '7abc0caa45be2b1fe8b4fe21bf41c91b'
+            self.MD5: '74c159a3e4c83d0aae5e6569c74a281f'
         }
         self.run_basic_test(test_source_dir, params)
 
