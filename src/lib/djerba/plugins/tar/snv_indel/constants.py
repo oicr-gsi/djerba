b156103afcef4cbd5f2b9da2a250df06457457d9
Aqsa Alam <97138161+AqsaAlam@users.noreply.github.com>
2023-08-10 16:45:02 -0400

    Create constants.py

---
+++ src/lib/djerba/plugins/tar/snv_indel/constants.py
@@ -0,0 +1,16 @@
+"""
+TAR-specific constants.
+"""
+
+GENES_TO_KEEP = ["BRCA2", 
+                 "BRCA1",
+                 "PALB2",
+                 "TP53",
+                 "APC",
+                 "EPCAM",
+                 "PMS2",
+                 "MLH1",
+                 "MSH2",
+                 "MSH6",
+                 "ABCB1",
+                 "CCNE1"]
