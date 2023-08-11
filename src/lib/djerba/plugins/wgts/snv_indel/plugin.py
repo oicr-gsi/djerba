93c4c1173ad0a65de1daf16171706cc58ac6b6bf
Felix Beaudry <Felix.Beaudry@oicr.on.ca>
2023-08-11 12:29:45 -0400

    test for wgts

--- src/lib/djerba/plugins/wgts/snv_indel/plugin.py
+++ src/lib/djerba/plugins/wgts/snv_indel/plugin.py
@@ -1,5 +1,5 @@
 """
-The purpose of this file is to prototype a plugin for TAR SNV Indel
+a plugin for WGTS SNV Indel
 """
 
 # IMPORTS
@@ -21,6 +21,7 @@ class main(plugin_base):
     PRIORITY = 100
     PLUGIN_VERSION = '1.0.0'
     TEMPLATE_NAME = 'snv_indel_template.html'
+    ASSAY = 'WGS'
     
     def configure(self, config):
       config = self.apply_defaults(config)
@@ -30,16 +31,16 @@ class main(plugin_base):
       wrapper = self.get_config_wrapper(config)  
       work_dir = self.workspace.get_work_dir()
       data = self.get_starting_plugin_data(wrapper, self.PLUGIN_VERSION)
-
+      oncotree = config[self.identifier]['oncotree_code']
       preprocess(config, work_dir, tar = False).run_R_code()
       mutations_file = os.path.join(self.work_dir, sic.MUTATIONS_EXTENDED)
       mutations_extended_file = os.path.join(self.work_dir, sic.MUTATIONS_EXTENDED_ONCOGENIC)
-      data = data_extractor(assay = assay).build_small_mutations_and_indels(mutations_extended_file)
+      data = data_extractor(self.ASSAY, oncotree).build_small_mutations_and_indels(mutations_extended_file)
       results = {
-           sic.VAF_PLOT: data_extractor(assay, oncotree).write_vaf_plot(self.work_dir),
-           sic.CLINICALLY_RELEVANT_VARIANTS: len(data),
-           sic.TOTAL_VARIANTS: data_extractor(assay, oncotree).read_somatic_mutation_totals(mutations_file),
-           sic.BODY: data
+          sic.BODY: data,
+          sic.CLINICALLY_RELEVANT_VARIANTS: len(data),
+          sic.TOTAL_VARIANTS: data_extractor(self.ASSAY, oncotree).read_somatic_mutation_totals(mutations_file),
+          sic.VAF_PLOT: data_extractor(self.ASSAY, oncotree).write_vaf_plot(self.work_dir)
       }
       data['results'] = results
       return data
@@ -51,11 +52,12 @@ class main(plugin_base):
     def specify_params(self):
       required = [
             'maf_file',
-            'oncotree_code',
-            'tcgacode',
+            'gep_file',
             'sequenza_file',
             'sequenza_gamma',
-            'sequenza_solution'
+            'sequenza_solution',
+            'oncotree_code',
+            'tcgacode',
             'tumour_id',
             'normal_id',
             'study_title'
---
+++ src/lib/djerba/plugins/wgts/snv_indel/test/plugin_test.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+"""
+Test of the WGTS small plugin
+AUTHOR: Felix Beaudry
+"""
+
+import os
+import unittest
+import tempfile
+
+from djerba.util.validator import path_validator
+from djerba.plugins.plugin_tester import PluginTester
+import djerba.plugins.wgts.snv_indel.plugin as snv_indel
+from djerba.core.workspace import workspace
+
+class TestWGTSsmallPlugin(PluginTester):
+
+    def setUp(self):
+        self.path_validator = path_validator()
+        self.maxDiff = None
+        self.tmp = tempfile.TemporaryDirectory(prefix='djerba_')
+        self.tmp_dir = self.tmp.name
+        sup_dir_var = 'DJERBA_TEST_DATA'
+        self.sup_dir = os.environ.get(sup_dir_var)
+
+    def testWGTSsmall(self):
+        test_source_dir = os.path.realpath(os.path.dirname(__file__))
+        json_location = os.path.join(self.sup_dir ,"snv_indel-plugin/report_json/snv_indel.wgs.json")
+        params = {
+            self.INI: 'snv_indel.ini',
+            self.JSON: json_location,
+            self.MD5: 'f8763747e45463c8163a8fe6d6c1956a'
+        }
+        self.run_basic_test(test_source_dir, params)
+
+if __name__ == '__main__':
+    unittest.main()
