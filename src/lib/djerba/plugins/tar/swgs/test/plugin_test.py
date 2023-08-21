deb5c247f32473679df8c3687492aa2df3e2d82c
Aqsa Alam <aalam@oicr.on.ca>
2023-08-21 17:04:52 -0400

    Testing for swgs, swgs and snv indel if statements for purity, html sentences edited

--- src/lib/djerba/plugins/tar/swgs/test/plugin_test.py
+++ src/lib/djerba/plugins/tar/swgs/test/plugin_test.py
@@ -22,11 +22,10 @@ class TestTarSNVIndelPlugin(PluginTester):
         self.tmp_dir = self.tmp.name
         
  
-        self.data_CNA = '/.mounts/labs/CGI/scratch/aalam/plugin_tests/snv-indel-plugin/data_CNA.txt'
-        self.data_CNA_onco = '/.mounts/labs/CGI/scratch/aalam/plugin_tests/snv-indel-plugin/data_CNA_oncoKBgenes_nonDiploid.txt'
-        self.provenance_output = '/.mounts/labs/CGI/scratch/aalam/plugin_tests/snv-indel-plugin/provenance_subset.tsv.gz'
-        self.json = '/.mounts/labs/CGI/scratch/aalam/plugin_tests/snv-indel-plugin/tar_snv_indel.json'
-        self.purity = '/.mounts/labs/CGI/scratch/aalam/plugin_tests/snv-indel-plugin/purity.txt'
+        self.provenance_output = '/.mounts/labs/CGI/scratch/aalam/plugin_tests/swgs-plugin/provenance_subset.tsv.gz'
+        self.purity = '/.mounts/labs/CGI/scratch/aalam/plugin_tests/swgs-plugin/purity.txt'
+        self.json = '/.mounts/labs/CGI/scratch/aalam/plugin_tests/swgs-plugin/tar_swgs.json'
+        
 
         sup_dir_var = 'DJERBA_TEST_DATA'
         self.sup_dir = os.environ.get(sup_dir_var)
@@ -35,16 +34,14 @@ class TestTarSNVIndelPlugin(PluginTester):
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
         
         # Copy files into the temporary directory
-        shutil.copy(self.data_CNA, self.tmp_dir)
-        shutil.copy(self.data_CNA_onco, self.tmp_dir)
         shutil.copy(self.provenance_output, self.tmp_dir)
         shutil.copy(self.purity, self.tmp_dir)
 
         json_location = self.json
         params = {
-            self.INI: 'data/tar_snv_indel.ini',
+            self.INI: 'data/tar_swgs.ini',
             self.JSON: json_location,
-            self.MD5: '4e3a4490ca2ba18d43a97453fb4cb129'
+            self.MD5: 'cb472dc9ec3dacfcd8ada05fe687fe1c'
         }
         self.run_basic_test(test_source_dir, params)
 
