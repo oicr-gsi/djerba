f20bce10ef4c8fbecc59e1436c9bae87c4ff5108
Aqsa Alam <aalam@oicr.on.ca>
2023-09-06 15:46:27 -0400

    supports input_params_helper

--- src/lib/djerba/plugins/supplement/plugin.py
+++ src/lib/djerba/plugins/supplement/plugin.py
@@ -1,6 +1,6 @@
 """Djerba plugin for pwgs supplement"""
 import logging
-
+import os
 from djerba.plugins.base import plugin_base, DjerbaPluginError
 import djerba.core.constants as core_constants
 from djerba.util.render_mako import mako_renderer
@@ -10,21 +10,33 @@ class main(plugin_base):
     DEFAULT_CONFIG_PRIORITY = 1000
     MAKO_TEMPLATE_NAME = 'supplementary_materials_template.html'
     SUPPLEMENT_DJERBA_VERSION = 0.1
-
+    INPUT_PARAMS_FILE = "input_params.json"
     ASSAY = "assay"
     
     def specify_params(self):
-        required = [
+        discovered = [
             self.ASSAY
         ]
-        for key in required:
-            self.add_ini_required(key)
+        for key in discovered:
+            self.add_ini_discovered(key)
         self.set_ini_default(core_constants.ATTRIBUTES, 'clinical')
 
     def configure(self, config):
         config = self.apply_defaults(config)
         wrapper = self.get_config_wrapper(config)
         wrapper.set_my_priorities(self.DEFAULT_CONFIG_PRIORITY)
+        
+        # If input_params.json exists, read it
+        work_dir = self.workspace.get_work_dir()
+        input_data_path = os.path.join(work_dir, self.INPUT_PARAMS_FILE)
+        if os.path.exists(input_data_path):
+            input_data = self.workspace.read_json(input_data_path)
+        else:
+            msg = "Could not find input_params.json"
+            #print(msg) <-- TO DO: have logger raise warning
+        
+        if wrapper.my_param_is_null(self.ASSAY):
+            wrapper.set_my_param(self.ASSAY, input_data[self.ASSAY])
         return config
 
     def extract(self, config):
@@ -36,7 +48,7 @@ class main(plugin_base):
             'attributes': wrapper.get_my_attributes(),
             'merge_inputs': {},
             'results': {
-                'assay': config[self.identifier]['assay']
+                'assay': config[self.identifier][self.ASSAY]
             },
             'version': str(self.SUPPLEMENT_DJERBA_VERSION)
         }
