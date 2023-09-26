05d3e2136df1ffbe3a545ad573166d7678a28f49
Aqsa Alam <aalam@oicr.on.ca>
2023-09-26 11:53:00 -0400

    removed input params helper and modified provenance helper to remove changes/match dev

--- src/lib/djerba/helpers/provenance_helper/test/helper_test.py
+++ src/lib/djerba/helpers/provenance_helper/test/helper_test.py
@@ -17,20 +17,20 @@ class TestProvenanceHelper(TestBase):
 
     CORE = 'core'
     HELPER_NAME = 'provenance_helper'
-    SUBSET_MD5 = '41c9288d5159f960f0193939a411a113'
-    SAMPLE_INFO_MD5 = '6eaf49a1c0e558b6861c328b963e9497'
+    SUBSET_MD5 = '62c0b00c42a352d9ce3c49aedb55e8e2'
+    SAMPLE_INFO_MD5 = '5d358d76c0013748b5fc34c52b6abe56'
     PATH_INFO_MD5 = 'f90fd360aeb81bf4d90905c65be905c6'
     
     def test(self):
-        data_dir = os.path.join(os.environ.get('DJERBA_TEST_DATA'), 'helpers', 'provenance')
+        data_dir = os.path.join(os.environ.get('DJERBA_TEST_DIR'), 'helpers', 'provenance')
         provenance_input = os.path.join(data_dir, 'provenance_input.tsv.gz')
         ws = workspace(self.tmp_dir)
         loader = helper_loader(logging.WARNING)
         helper_main = loader.load(self.HELPER_NAME, ws)
         config = helper_main.get_expected_config()
         config.add_section(self.CORE)
-        config.set(self.HELPER_NAME, 'project', 'PASS01')
-        config.set(self.HELPER_NAME, 'donor', 'PANX_1500')
+        config.set(self.HELPER_NAME, 'study_title', 'PASS01')
+        config.set(self.HELPER_NAME, 'root_sample_name', 'PANX_1500')
         config.set(self.HELPER_NAME, 'provenance_input_path', provenance_input)
         config = helper_main.configure(config)
         subset_path = os.path.join(self.tmp_dir, helper_main.PROVENANCE_OUTPUT)
