c0de90bfb91a24be6f83c827ad7590d355769d08
Iain Bancarz PhD <1695061+iainrb@users.noreply.github.com>
2023-10-10 15:35:55 -0400

    Update CNV test, and modify Sequenza class to accept a simplified test data set

--- src/lib/djerba/plugins/cnv/test/plugin_test.py
+++ src/lib/djerba/plugins/cnv/test/plugin_test.py
@@ -1,43 +1,49 @@
 #! /usr/bin/env python3
 
 """
-Test of the WGTS small plugin
-AUTHOR: Felix Beaudry
+Test of the WGTS CNV plugin
 """
 
 import os
-import unittest
+import string
 import tempfile
-
+import unittest
+from shutil import copy
 from djerba.util.validator import path_validator
 from djerba.plugins.plugin_tester import PluginTester
-import djerba.plugins.wgts.cnv.plugin as cnv
+from djerba.plugins.cnv.plugin import main as cnv
 from djerba.core.workspace import workspace
 
-class TestWGTSsmallPlugin(PluginTester):
+class TestWgtsCnv(PluginTester):
 
-    def setUp(self):
-        self.path_validator = path_validator()
-        self.maxDiff = None
-        self.tmp = tempfile.TemporaryDirectory(prefix='djerba_')
-        self.tmp_dir = self.tmp.name
-        sup_dir_var = 'DJERBA_TEST_DATA'
-        self.sup_dir = os.environ.get(sup_dir_var)
+    INI_NAME = 'cnv.ini'
+    JSON_NAME = 'cnv.json'
 
-    def testWGTScnv(self):
+    def testWgtsCnv(self):
+        sup_dir = os.environ.get('DJERBA_TEST_DATA')
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
-        json_location = os.path.join(self.sup_dir ,"wgs-cnv-plugin/report_json/cnv.wgs.json")
+        json_location = os.path.join(sup_dir, "plugins/cnv/cnv.json")
+        sequenza_filename = 'OCT_011488_Lu_M_WG_OCT_011488-TS_results.gamma400.zip'
+        sequenza_path = os.path.join(sup_dir, 'plugins', 'cnv', sequenza_filename)
+        with open(os.path.join(test_source_dir, self.INI_NAME)) as in_file:
+            template_str = in_file.read()
+        template = string.Template(template_str)
+        ini_str = template.substitute({'SEQUENZA_PATH': sequenza_path})
+        input_dir = os.path.join(self.get_tmp_dir(), 'input')
+        os.mkdir(input_dir)
+        with open(os.path.join(input_dir, self.INI_NAME), 'w') as ini_file:
+            ini_file.write(ini_str)
+        copy(os.path.join(test_source_dir, self.JSON_NAME), input_dir)
         params = {
-            self.INI: 'cnv.ini',
-            self.JSON: json_location,
+            self.INI: self.INI_NAME,
+            self.JSON: self.JSON_NAME,
             self.MD5: 'e8d5c9777e4dbd6162105cd189cb40bf'
         }
-        self.run_basic_test(test_source_dir, params)
+        self.run_basic_test(input_dir, params)
 
     def redact_json_data(self, data):
         """replaces empty method from testing.tools"""
-        for key in ['cnv_plot']:
-            del data['plugins']['wgts.cnv']['results'][key]
+        del data['plugins']['wgts.cnv']['results']['cnv_plot']
         return data 
 
 if __name__ == '__main__':
