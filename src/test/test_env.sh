d705db293349cd73917e58797f655e9c70ab7167
Felix Beaudry <Felix.Beaudry@oicr.on.ca>
2023-11-01 15:14:26 -0400

    replaced home with djerba_home

--- src/test/test_env.sh
+++ src/test/test_env.sh
@@ -4,12 +4,15 @@
 # assumes djerba and djerba_test_data are in ~/git
 # update module and filename as necessary for additional dev releases
 
-module load djerba
-export PYTHONPATH=${HOME}/git/djerba/src/lib:$PYTHONPATH
-export PATH=${HOME}/git/djerba/src/bin:$PATH
-export DJERBA_BASE_DIR=${HOME}/git/djerba/src/lib/djerba
-export DJERBA_TEST_DATA=${HOME}/git/djerba_test_data
-export DJERBA_GSICAPBENCH_DATA=/.mounts/labs/CGI/gsi/djerba_test/GSICAPBENCH_djerba_latest
-export DJERBA_BASE_DIR=$HOME/git/djerba/src/lib/djerba
-export DJERBA_DATA_DIR=$HOME/git/djerba/src/lib/djerba/data
-export DJERBA_PRIVATE_DIR=$HOME/workspace/djerba/env/private
+if [ -z "${DJERBA_SOURCE_DIR}" ]; then
+	echo "Must set environment variable DJERBA_SOURCE_DIR"
+else
+	module load djerba
+	export PYTHONPATH=${DJERBA_SOURCE_DIR}/src/lib:$PYTHONPATH
+	export PATH=${DJERBA_SOURCE_DIR}/src/bin:$PATH
+	export DJERBA_BASE_DIR=${DJERBA_SOURCE_DIR}/src/lib/djerba
+	export DJERBA_DATA_DIR=${DJERBA_SOURCE_DIR}/src/lib/djerba/data
+	export DJERBA_PRIVATE_DIR=${HOME}/workspace/djerba/env/private
+	export DJERBA_TEST_DATA=${HOME}/git/djerba_test_data
+	export DJERBA_GSICAPBENCH_DATA=/.mounts/labs/CGI/gsi/djerba_test/GSICAPBENCH_djerba_latest
+fi
