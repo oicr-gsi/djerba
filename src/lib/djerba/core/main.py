a934570b865d0dce8d4cb465076bff00bbfdecde
iainrb <1695061+iainrb@users.noreply.github.com>
2023-11-03 15:41:56 -0400

    Fix logging statements

--- src/lib/djerba/core/main.py
+++ src/lib/djerba/core/main.py
@@ -51,10 +51,11 @@ class main(core_base):
     def _check_author_name(self, data):
         if data[cc.CORE][cc.AUTHOR] == cc.DEFAULT_AUTHOR:
             msg = 'Default author name "{}" is in use; '.format(cc.DEFAULT_AUTHOR)+\
-                "if this is a production report, name MUST be set to an authorized individual"
+                "if this is a production report, name MUST be set "+\
+                "to an authorized individual"
             self.logger.warning(msg)
         else:
-            msg = "User-configured author name is '{0}'".format(core_params[cc.AUTHOR])
+            msg = "User-configured author name is '{0}'".format(data[cc.CORE][cc.AUTHOR])
             self.logger.debug(msg)
 
     def _get_render_priority(self, plugin_data):
