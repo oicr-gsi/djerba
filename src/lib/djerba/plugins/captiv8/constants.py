965da75e4c1e68effff9b305c3a8baedaa395377
Felix Beaudry <Felix.Beaudry@oicr.on.ca>
2023-11-22 14:48:39 -0500

    removed captiv8

--- src/lib/djerba/plugins/captiv8/constants.py
+++
@@ -1,8 +0,0 @@
-# constants for the CNV plugin
-
-import djerba.core.constants as core_constants
-import djerba.util.oncokb.constants as oncokb_constants
-
-CAPTIV8_PLOT = 'captiv8_base64'
-ELIGIBILITY = 'eligibility'
-SCORE = 'captiv8-score'
