0cb0c78c5acaec3131a70154cecefc64406304bd
Aqsa Alam <aalam@oicr.on.ca>
2023-12-05 16:52:55 -0500

    Fixed case overview tests and tests for input params helper and tar input params helper

--- src/lib/djerba/plugins/case_overview/test/plugin_test.py
+++ src/lib/djerba/plugins/case_overview/test/plugin_test.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 
-"""Test of the patient info plugin"""
+"""Test of the case overview plugin"""
 
-import logging
 import os
+import shutil
+import logging
 import string
 import unittest
 import tempfile
@@ -21,26 +22,28 @@ class TestCaseOverview(PluginTester):
         self.tmp_dir = self.tmp.name
         sup_dir_var = 'DJERBA_TEST_DATA'
         self.sup_dir = os.environ.get(sup_dir_var)
+        self.sample_json = os.path.join(self.sup_dir, "plugins/case_overview/sample_info.json")
 
     def testCaseOverviewWGTS(self):
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
-        json_location = os.path.join(self.sup_dir ,"case-overview-plugin/case_overview_WGTS.json")
+        json_location = os.path.join(self.sup_dir ,"plugins/case_overview/case_overview_WGTS.json")
+        shutil.copy(self.sample_json, self.tmp_dir)
 
         params = {
             self.INI: 'case_overview_WGTS.ini',
             self.JSON: json_location,
-            self.MD5: 'f8763747e45463c8163a8fe6d6c1956a' # TO CHANGE
+            self.MD5: '7ca69fb48132371b2eb44e155caf45fe'
         }
         self.run_basic_test(test_source_dir, params)
 
     def testCaseOverviewTAR(self):
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
-        json_location = os.path.join(self.sup_dir ,"case-overview-plugin/case_overview_TAR.json")
+        json_location = os.path.join(self.sup_dir ,"plugins/case_overview/case_overview_TAR.json")
 
         params = {
             self.INI: 'case_overview_TAR.ini',
             self.JSON: json_location,
-            self.MD5: 'f8763747e45463c8163a8fe6d6c1956a' # TO CHANGE
+            self.MD5: '28efb7d0c9bc16a672990c0ca1451a5d'
         }
         self.run_basic_test(test_source_dir, params)
 
