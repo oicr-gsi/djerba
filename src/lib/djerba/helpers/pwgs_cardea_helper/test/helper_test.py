2593978f85a895526419c09466e44229d41dfaa1
Felix Beaudry <Felix.Beaudry@oicr.on.ca>
2024-01-04 17:26:34 -0500

    cardea helper functional

--- src/lib/djerba/helpers/pwgs_cardea_helper/test/helper_test.py
+++ src/lib/djerba/helpers/pwgs_cardea_helper/test/helper_test.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-"""Test of the provenance helper"""
+"""Test of the pwgs cardea helper"""
 
 import logging
 import os
--- src/lib/djerba/plugins/sample/test/plugin_test.py
+++ src/lib/djerba/plugins/sample/test/plugin_test.py
@@ -22,7 +22,12 @@ class TestWgtsSamplePlugin(PluginTester):
         self.tmp_dir = self.tmp.name
         sup_dir_var = 'DJERBA_TEST_DATA'
         self.sup_dir = os.environ.get(sup_dir_var)
-         
+
+    def testPreprocessSNVcount(self):
+        snv_count_expected_location = os.path.join(self.sup_dir ,"plugins/pwgs/snv.txt")
+        snv_count = sample.main.preprocess_snv_count(self, snv_count_expected_location)
+        self.assertEqual(snv_count, 21000)
+        
     def testWgtsSample(self):
         test_source_dir = os.path.realpath(os.path.dirname(__file__))
         json_location = os.path.join(self.sup_dir ,"plugins/sample/report_json/sample.json")
