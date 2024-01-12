3b68d8935d880140042fc5af47e1a0be4393f5d5
iainrb <1695061+iainrb@users.noreply.github.com>
2024-01-12 16:31:22 -0500

    Add mini_djerba.py script to setup

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         'src/bin/benchmark.py',
         'src/bin/djerba.py',
         'src/bin/generate_ini.py',
+        'src/bin/mini_djerba.py',
         'src/bin/sequenza_explorer.py',
         'src/bin/update_oncokb_cache.py'
     ],
