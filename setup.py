2521f1ab1d8a7793c2b7fa9a5ecccb8068984d4c
Iain Bancarz PhD <1695061+iainrb@users.noreply.github.com>
2024-01-12 10:02:41 -0500

    Fix clinical report header and patient info setup (#316) (#317)
    * Fix clinical report header and patient info setup; update version and changelog for release 1.2.1

--- setup.py
+++ setup.py
@@ -90,6 +90,7 @@ setup(
         'djerba.plugins.fusion': install_wildcards,
         'djerba.plugins.genomic_landscape': install_wildcards,
         'djerba.plugins.hrd': install_wildcards,
+        'djerba.plugins.patient_info': install_wildcards,
         'djerba.plugins.pwgs.analysis': install_wildcards,
         'djerba.plugins.pwgs.case_overview': install_wildcards,
         'djerba.plugins.pwgs.sample': install_wildcards,
