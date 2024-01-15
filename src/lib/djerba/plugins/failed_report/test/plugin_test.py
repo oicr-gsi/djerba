fd359abf32050694086bc452ba6a72232704090e
Aqsa Alam <aalam@oicr.on.ca>
2024-01-15 11:40:21 -0500

    Fixed plugin test

--- src/lib/djerba/plugins/failed_report/test/plugin_test.py
+++ src/lib/djerba/plugins/failed_report/test/plugin_test.py
@@ -23,7 +23,7 @@ class TestFailedReportPlugin(PluginTester):
         params = {
             self.INI: 'failed_report.ini',
             self.JSON: json_location,
-            self.MD5: 'cfd14cdd3aa520975986a1cb71c0cb90'
+            self.MD5: 'b3021c11923d960754ab90012cc4da7d'
         }
         self.run_basic_test(test_source_dir, params)
 
