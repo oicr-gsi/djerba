0912749307bb7088ea2b382c249b88da5ff35e92
Felix Beaudry <Felix.Beaudry@oicr.on.ca>
2024-01-24 12:10:40 -0500

    moved supplementary info

--- src/lib/djerba/util/html.py
+++ src/lib/djerba/util/html.py
@@ -51,7 +51,7 @@ class html_builder:
         return markdown(markdown_string)
     
     @staticmethod
-    def section_cells_begin(section_title, is_main):
+    def section_cells_begin(section_title, is_main, is_top=False):
         # begin a cell structure with title in left-hand cell, body in right-hand cell
         if is_main:
             class_suffix = 'main'
@@ -59,9 +59,14 @@ class html_builder:
         else:
             class_suffix = 'supp'
             rank = '3'
-        template = '<hr class="big-white-line" ><div class="twocell{0}">'+\
-            '<div class="oneoftwocell{0}"><h{1}>{2}</h{1}></div>'+\
-            '<div class="twooftwocell{0}" ><hr class="big-line" >'
+        if is_top:
+            template = '<hr class="big-white-line" ><div class="twocell{0}">'+\
+                '<div class="oneoftwocell{0}"><h{1}>{2}</h{1}></div>'+\
+                '<div class="twooftwocell{0}" >'
+        else:
+            template = '<hr class="big-white-line" ><div class="twocell{0}">'+\
+                '<div class="oneoftwocell{0}"><h{1}>{2}</h{1}></div>'+\
+                '<div class="twooftwocell{0}" ><hr class="big-line" >'
         cell_begin = template.format(class_suffix, rank, section_title)
         return cell_begin
 
