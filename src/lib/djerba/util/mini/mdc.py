b9cb789078f9966da530789af7e0670f54fe7c55
iainrb <1695061+iainrb@users.noreply.github.com>
2024-02-09 18:03:50 -0500

    Support new supplement.body params in mini-djerba

--- src/lib/djerba/util/mini/mdc.py
+++ src/lib/djerba/util/mini/mdc.py
@@ -7,22 +7,35 @@ import logging
 import re
 import djerba.core.constants as core_constants
 from configparser import ConfigParser, NoOptionError
-from djerba.plugins.patient_info.plugin import main as plugin
+from time import strftime
+from djerba.plugins.patient_info.plugin import main as patient_info_plugin
+from djerba.plugins.supplement.body.plugin import main as supplement_plugin
 from djerba.util.logger import logger
 from djerba.util.validator import path_validator
 
 class mdc(logger):
 
+    MDC = 'mdc'
+    MDC_HEADER = '['+MDC+']'
+
     PATIENT_INFO_KEYS = [
-        plugin.PATIENT_NAME,
-        plugin.PATIENT_DOB,
-        plugin.PATIENT_SEX,
-        plugin.REQ_EMAIL,
-        plugin.PHYSICIAN_LICENCE,
-        plugin.PHYSICIAN_NAME,
-        plugin.PHYSICIAN_PHONE,
-        plugin.PHYSICIAN_HOSPITAL
+        patient_info_plugin.PATIENT_NAME,
+        patient_info_plugin.PATIENT_DOB,
+        patient_info_plugin.PATIENT_SEX,
+        patient_info_plugin.REQ_EMAIL,
+        patient_info_plugin.PHYSICIAN_LICENCE,
+        patient_info_plugin.PHYSICIAN_NAME,
+        patient_info_plugin.PHYSICIAN_PHONE,
+        patient_info_plugin.PHYSICIAN_HOSPITAL
     ]
+
+    SUPPLEMENT_KEYS = [
+        supplement_plugin.REPORT_SIGNOFF_DATE,
+        supplement_plugin.GENETICIST,
+        supplement_plugin.GENETICIST_ID
+    ]
+
+    CONFIG_KEYS = PATIENT_INFO_KEYS + SUPPLEMENT_KEYS
 
     def __init__(self, log_level=logging.WARNING, log_path=None):
         self.log_level = log_level
@@ -33,14 +46,14 @@ class mdc(logger):
     def read(self, in_path):
         """
         Input: Path to a .mdc config file
-        Output: Dictionary of patient info, and string containing summary text
+        Output: Dictionaries of params, and string containing summary text
         """
         self.validator.validate_input_file(in_path)
         with open(in_path, encoding=core_constants.TEXT_ENCODING) as in_file:
             in_lines = in_file.readlines()
         self.sanity_check(in_lines)
         # Read INI lines up to a ### marker, then switch to the free text section
-        ini_lines = ["[patient_info]\n"]
+        ini_lines = [self.MDC_HEADER+"\n"] # temporary header for INI string parsing
         text_lines = []
         ini_section = True
         for line in in_lines:
@@ -54,14 +67,15 @@ class mdc(logger):
         config = ConfigParser()
         config.read_string(''.join(ini_lines))
         try:
-            patient_info = {k:config.get('patient_info', k) for k in self.PATIENT_INFO_KEYS}
+            patient_info = {k:config.get(self.MDC, k) for k in self.PATIENT_INFO_KEYS}
+            supplement = {k:config.get(self.MDC, k) for k in self.SUPPLEMENT_KEYS}
         except NoOptionError as err:
             msg = "Missing one or more key=value pairs. MDC file must have values "+\
-                "for the following keys: {0}".format(', '.join(self.PATIENT_INFO_KEYS))
+                "for the following keys: {0}".format(', '.join(self.CONFIG_KEYS))
             self.logger.error(msg)
             raise MDCFormatError(msg) from err
         self.logger.info("Read MDC file from {0}".format(in_path))
-        return [patient_info, text]
+        return [patient_info, supplement, text]
 
     def sanity_check(self, lines):
         """
@@ -97,14 +111,21 @@ class mdc(logger):
             self.logger.error(err)
             raise MDCFormatError(err)
 
-    def write(self, out_path, patient_info, text):
+    def write(self, out_path, patient_info, supplement, text, auto_signoff_date=True):
         """
-        Input: Dictionary of patient info, and string containing summary text
+        Input: Dictionaries of patient info & supplement, and string containing summary text
+        Dictionaries may contain additional keys, which will be ignored
         """
         self.validator.validate_output_file(out_path)
         with open(out_path, 'w', encoding=core_constants.TEXT_ENCODING) as out_file:
             for key in self.PATIENT_INFO_KEYS:
                 value = patient_info.get(key)
+                print("{0} = {1}".format(key, value), file=out_file)
+            for key in self.SUPPLEMENT_KEYS:
+                if key == supplement_plugin.REPORT_SIGNOFF_DATE and auto_signoff_date:
+                    value = strftime('%Y/%m/%d')
+                else:
+                    value = supplement.get(key)
                 print("{0} = {1}".format(key, value), file=out_file)
             print("\n###\n", file=out_file)
             print(text, file=out_file)
