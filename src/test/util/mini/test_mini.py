b3743fb389000e13b74a2abf9b46f595e75cf30e
iainrb <1695061+iainrb@users.noreply.github.com>
2024-02-09 18:05:55 -0500

    Revert "Support new supplement.body params in mini-djerba"
    This reverts commit b9cb789078f9966da530789af7e0670f54fe7c55.
    Want to make these changes via a PR instead.

--- src/test/util/mini/test_mini.py
+++ src/test/util/mini/test_mini.py
@@ -4,13 +4,11 @@ import json
 import logging
 import os
 import unittest
-from copy import deepcopy
 from subprocess import CalledProcessError
 from time import strftime
 
 from djerba.core.main import DjerbaVersionMismatchError
 from djerba.plugins.patient_info.plugin import main as patient_info_plugin
-from djerba.plugins.supplement.body.plugin import main as supplement_plugin
 from djerba.util.mini.main import main
 from djerba.util.mini.mdc import mdc, MDCFormatError
 from djerba.util.testing.tools import TestBase
@@ -18,18 +16,11 @@ from djerba.util.subprocess_runner import subprocess_runner
 
 class TestMDC(TestBase):
 
-    SUPPLEMENT_EXPECTED = {
-        supplement_plugin.REPORT_SIGNOFF_DATE: '2023/12/01',
-        supplement_plugin.GENETICIST: supplement_plugin.GENETICIST_DEFAULT,
-        supplement_plugin.GENETICIST_ID: supplement_plugin.GENETICIST_ID_DEFAULT
-    }
-
     def test_read(self):
         test_dir = os.path.dirname(os.path.realpath(__file__))
         test_file = os.path.join(test_dir, 'config.mdc')
-        patient_info, supplement, text = mdc().read(test_file)
+        patient_info, text = mdc().read(test_file)
         self.assertEqual(patient_info, patient_info_plugin.PATIENT_DEFAULTS)
-        self.assertEqual(supplement, self.SUPPLEMENT_EXPECTED)
         self.assertEqual(text, 'Hello, world!')
 
     def test_read_fail(self):
@@ -51,18 +42,11 @@ class TestMDC(TestBase):
         patient_info['patient_name'] = 'Smith, John'
         patient_info['physician_name'] = 'Doe, Jane'
         text = 'Lorem ipsum dolor sit amet'
-        supplement = {
-            supplement_plugin.REPORT_SIGNOFF_DATE: strftime('%Y/%m/%d'),
-            supplement_plugin.GENETICIST: 'Jones, Jennifer',
-            supplement_plugin.GENETICIST_ID: supplement_plugin.GENETICIST_ID_DEFAULT
-        }
-        mdc().write(out_path, patient_info, supplement, text)
+        mdc().write(out_path, patient_info, text)
         self.assertTrue(os.path.isfile(out_path))
-        patient_info_new, supplement_new, text_new = mdc().read(out_path)
+        patient_info_new, text_new = mdc().read(out_path)
         self.assertEqual(patient_info, patient_info_new)
         self.assertEqual(patient_info_new['patient_name'], 'Smith, John')
-        self.assertEqual(supplement, supplement_new)
-        self.assertEqual(supplement[supplement_plugin.GENETICIST], 'Jones, Jennifer')
         self.assertEqual(text, text_new)
 
 class TestMiniBase(TestBase):
@@ -71,7 +55,7 @@ class TestMiniBase(TestBase):
 
     def assert_MDC(self, out_path):
         self.assertTrue(os.path.isfile(out_path))
-        self.assertEqual(self.getMD5(out_path), 'd9ee2ef8009be095977eaa1f7dcb1203')
+        self.assertEqual(self.getMD5(out_path), '48efe0ce5121a1878ebfc04f143f49cc')
 
     def assert_render(self):
         html_path = os.path.join(self.tmp_dir, 'placeholder_report.clinical.html')
@@ -81,7 +65,7 @@ class TestMiniBase(TestBase):
         with open(html_path) as html_file:
             original = html_file.read()
         redacted = self.redact_html(original)
-        self.assertEqual(self.getMD5_of_string(redacted), '1816cb8d7bc38c8912b8b044baa2f5eb')
+        self.assertEqual(self.getMD5_of_string(redacted), '90cb5c19d578c8e75348d6515486fa83')
 
     def assert_update(self):
         html_path = os.path.join(self.tmp_dir, 'placeholder_report.clinical.html')
@@ -92,7 +76,7 @@ class TestMiniBase(TestBase):
         with open(html_path) as html_file:
             original = html_file.read()
         redacted = self.redact_html(original)
-        self.assertEqual(self.getMD5_of_string(redacted), 'e8447ab958897b78685a9fe283f1ebe1')
+        self.assertEqual(self.getMD5_of_string(redacted), '315487573e94112bf4a847b7aac52822')
         with open(json_out) as json_file:
             json_data = json.loads(json_file.read())
         self.assertEqual(json_data['core']['extract_time'], '2023-12-20_21:38:10Z')
