79249a067461492fcc811474b8a41066094c5364
iainrb <1695061+iainrb@users.noreply.github.com>
2024-03-08 16:37:01 -0500

    Remove unnecessary file

--- src/lib/djerba/util/vaf.py
+++
@@ -1,9 +0,0 @@
-"""
-Extremely simple function used to parse VAF from a MAF file row in eg. csv.DictReader
-Used in VAF helper and snv/indel plugin, placed here to ensure consistency
-"""
-
-def get_tumour_vaf(row):
-    vaf = row['tumour_vaf']
-    vaf = int(round(float(vaf), 2)*100)
-    return vaf
