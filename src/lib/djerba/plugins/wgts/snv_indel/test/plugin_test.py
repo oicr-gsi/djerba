9b2fc2112af3a45fc0a4a6b00de831b865d86899
Aqsa Alam <aalam@oicr.on.ca>
2024-03-20 15:27:17 -0400

    Added test for when cnv data is missing

--- src/lib/djerba/plugins/wgts/snv_indel/test/plugin_test.py
+++ src/lib/djerba/plugins/wgts/snv_indel/test/plugin_test.py
@@ -19,8 +19,9 @@ class TestSnvIndelPlugin(PluginTester):
 
     INI_NAME = 'snv_indel.ini'
     JSON_NAME = 'snv_indel.json'
+    JSON_NAME_NO_CNV = 'snv_indel_no_cnv.json'
 
-    def testSnvIndel(self):
+    def testSnvIndelWithCNV(self):
 
         data_dir_root = directory_finder().get_test_dir()
         data_dir = os.path.join(data_dir_root, 'plugins', 'wgts', 'snv_indel')
@@ -52,6 +53,35 @@ class TestSnvIndelPlugin(PluginTester):
             self.INI: self.INI_NAME,
             self.JSON: self.JSON_NAME,
             self.MD5: '9064ac742f683240c3f0d3025bd58366'
+        }
+        self.run_basic_test(input_dir, params, work_dir=work_dir)
+
+    def testSnvIndelNoCNV(self):
+
+        data_dir_root = directory_finder().get_test_dir()
+        data_dir = os.path.join(data_dir_root, 'plugins', 'wgts', 'snv_indel')
+        test_source_dir = os.path.realpath(os.path.dirname(__file__))
+        maf_filename = 'PANX_1391_Lv_M_WG_100-NH-020_LCM3.filter.deduped.realigned.'+\
+            'recalibrated.mutect2.filtered.subset.maf.gz'
+        maf_path = os.path.join(data_dir, maf_filename)
+        expression_path = os.path.join(data_dir, 'data_expression_percentile_tcga.json')
+        with open(os.path.join(test_source_dir, self.INI_NAME)) as in_file:
+            template_str = in_file.read()
+        template = string.Template(template_str)
+        ini_str = template.substitute( {'MAF_PATH': maf_path})
+        tmp_dir = self.get_tmp_dir()
+        input_dir = os.path.join(tmp_dir, 'input')
+        os.mkdir(input_dir)
+        work_dir = os.path.join(tmp_dir, 'work')
+        os.mkdir(work_dir)
+        copy(expression_path, work_dir)
+        with open(os.path.join(input_dir, self.INI_NAME), 'w') as ini_file:
+            ini_file.write(ini_str)
+        copy(os.path.join(data_dir, self.JSON_NAME_NO_CNV), input_dir)
+        params = {
+            self.INI: self.INI_NAME,
+            self.JSON: self.JSON_NAME_NO_CNV,
+            self.MD5: '5909c8a50497adb6b615cb56435e220f'
         }
         self.run_basic_test(input_dir, params, work_dir=work_dir)
 
