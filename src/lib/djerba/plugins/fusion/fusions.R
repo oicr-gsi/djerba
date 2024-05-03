0e0d0199622aaee85f312de054824e99ea0d5833
Oumaima Hamza <ohamza@oicr.on.ca>
2024-05-03 17:00:34 -0400

    debugging

--- src/lib/djerba/plugins/fusion/fusions.R
+++ src/lib/djerba/plugins/fusion/fusions.R
@@ -171,6 +171,11 @@ processFusions <- function(datafile, readfilt, entrfile, arribafile ){
    
    df_cbio <- df_cbio[!duplicated(df_cbio),]
 
+   print("Column names in data df")
+   print(names(data))
+   print("Column names in 'df_cbio' before merging")
+   print(names(df_cbio))
+
    df_cbio <- merge(df_cbio, data[, c("Sample", "break1_chromosome", "break1_position_start", "break1_position_end")], by="Tumor_Sample_Barcode", all.x=TRUE)
    colnames(df_cbio) <- header
 
