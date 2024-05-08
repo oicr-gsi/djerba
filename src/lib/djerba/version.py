9179a1b45fd9d9d60094406bd2463167594b2fc3
iainrb <1695061+iainrb@users.noreply.github.com>
2024-05-08 17:01:38 -0400

    Update changelog and version for release 1.6.0

--- src/lib/djerba/version.py
+++ src/lib/djerba/version.py
@@ -3,7 +3,7 @@
 # 2) we can import it in setup.py for the same reason
 # 3) it only needs to be stored in one place
 # See https://stackoverflow.com/a/16084844
-__version__ = '1.5.6'
+__version__ = '1.6.0'
 
 def get_djerba_version():
     return __version__
