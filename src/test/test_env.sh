7041adf5861905ec883f9bec1f03fd6ff62039dd
iainrb <1695061+iainrb@users.noreply.github.com>
2024-07-09 11:24:09 -0400

    Remove test dir

--- src/test/test_env.sh
+++ src/test/test_env.sh
@@ -11,8 +11,6 @@ fi
 # do sanity checking, then export the test variables
 if [ ! -d "${DJERBA_SOURCE_DIR}" ]; then
     echo "DJERBA_SOURCE_DIR '$DJERBA_SOURCE_DIR' does not exist"
-elif [ ! -d "${DJERBA_TEST_DIR}" ]; then
-    echo "DJERBA_TEST_DIR '$DJERBA_TEST_DIR' does not exist"
 elif [ -z "${DJERBA_BASE_DIR}" ]; then
     echo "Must load the Djerba environment module; first update MODULEPATH if necessary"
 else
@@ -21,7 +19,4 @@ else
     export PATH=${DJERBA_SOURCE_DIR}/src/bin:$PATH
     export DJERBA_BASE_DIR=${DJERBA_SOURCE_DIR}/src/lib/djerba
     export DJERBA_RUN_DIR=${DJERBA_BASE_DIR}/data
-    # DJERBA_TEST_DIR is set by the environment module
-    export DJERBA_TEST_DATA=$DJERBA_TEST_DIR # deprecated, but still used in some tests
-    # DJERBA_PRIVATE_DIR is set by the environment module
 fi
