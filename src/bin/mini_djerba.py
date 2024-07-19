7801176ecefe0d61358b5ff972fe1f3fd58633fe
iainrb <1695061+iainrb@users.noreply.github.com>
2024-07-19 12:39:42 -0400

    Remove unwanted code, fix assay discovery and error handling

--- src/bin/mini_djerba.py
+++ src/bin/mini_djerba.py
@@ -12,7 +12,6 @@ import logging
 from tempfile import TemporaryDirectory
 import djerba.util.mini.constants as constants
 from djerba.util.mini.main import main, arg_processor, MiniDjerbaScriptError
-from djerba.util.mini.mdc import MDCFormatError
 from djerba.version import get_djerba_version
 
 def get_parser():
@@ -30,11 +29,6 @@ def get_parser():
     setup_parser = subparsers.add_parser(constants.SETUP, help='Set up config files')
     setup_parser.add_argument('-j', '--json', metavar='PATH', help='Existing report JSON. Optional, if not given will generate a blank config file.')
     setup_parser.add_argument('-o', '--out_dir', metavar='PATH', default='.', help='Output path. Optional, defaults to the current working directory.')
-    render_parser = subparsers.add_parser(constants.RENDER, help='Render a JSON report file to HTML and optional PDF, with no changes')
-    render_parser.add_argument('-j', '--json', metavar='PATH', required=True, help='Path to the Djerba report JSON file to be updated')
-    render_parser.add_argument('-o', '--out-dir', metavar='DIR', default='.', help='Directory for output files. Optional, defaults to the current directory.')
-    render_parser.add_argument('-w', '--work-dir', metavar='PATH', help='Path to workspace directory; optional, defaults to a temporary directory')
-    render_parser.add_argument('--no-pdf', action='store_true', help='Do not generate PDF output from HTML')
     report_parser = subparsers.add_parser(constants.REPORT, help='Generate HTML/PDF documents from a report JSON file')
     report_parser.add_argument('-f', '--force', action='store_true', help='Force update of mismatched plugin versions')
     report_parser.add_argument('-i', '--ini', metavar='PATH', help='Path to a mini-Djerba INI config file; optional')
@@ -43,14 +37,6 @@ def get_parser():
     report_parser.add_argument('-s', '--summary', metavar='PATH', help='Path to a summary text file; optional')
     report_parser.add_argument('-w', '--work-dir', metavar='PATH', help='Path to workspace directory; optional, defaults to a temporary directory')
     report_parser.add_argument('--no-pdf', action='store_true', help='Do not generate PDF output from HTML')
-    update_parser = subparsers.add_parser(constants.UPDATE, help='Render a JSON file to HTML and optional PDF, with updates from an MDC file')
-    update_parser.add_argument('-c', '--config', metavar='PATH', required=True, help='Path to an MDC (mini-Djerba config) file')
-    update_parser.add_argument('-f', '--force', action='store_true', help='Force update of mismatched plugin versions')
-    update_parser.add_argument('-j', '--json', metavar='PATH', required=True, help='Path to the Djerba report JSON file to be updated')
-    update_parser.add_argument('-o', '--out-dir', metavar='DIR', default='.', help='Directory for output files. Optional, defaults to the current directory.')
-    update_parser.add_argument('-u', '--write-json', action='store_true', help='Write updated JSON to the output directory')
-    update_parser.add_argument('-w', '--work-dir', metavar='PATH', help='Path to workspace directory; optional, defaults to a temporary directory')
-    update_parser.add_argument('--no-pdf', action='store_true', help='Do not generate PDF output from HTML')
     return parser
 
 if __name__ == '__main__':
@@ -77,8 +63,8 @@ if __name__ == '__main__':
         else:
             with TemporaryDirectory(prefix='mini_djerba_') as tmp_dir:
                 main(tmp_dir, ap.get_log_level(), ap.get_log_path()).run(args)
-    except MDCFormatError as err:
-        error_message = "Error reading the -c/--config file: {0}".format(err)
+    except MiniDjerbaScriptError as err:
+        error_message = "Error running Mini-Djerba: {0}".format(err)
         if not args.silent:
             raise
     except OSError as err:
@@ -86,7 +72,7 @@ if __name__ == '__main__':
         if not args.silent:
             raise
     except Exception as err:
-        error_message = "Unexpected Mini-Djerba error! Run with --verbose or --debug for details.\n"+\
+        error_message = "Unexpected Mini-Djerba error! Run 'mini_djerba --debug [mode] [options]' for details.\n"+\
             "If errors persist:\n"+\
             "- Email gsi@oicr.on.ca\n"+\
             "- Please DO NOT include personal health information (PHI)"
--- src/lib/djerba/util/mini/main.py
+++ src/lib/djerba/util/mini/main.py
@@ -32,29 +32,9 @@ class main(main_base):
         supplement_plugin.GENETICIST_ID
     ]
 
-    def get_supplement_params(self, mdc_supplement, data):
-        # want to configure the supplement.body plugin with new keys from MDC:
-        # - report_signoff_date (defaults to current date)
-        # - clinical_geneticist_name
-        # - clinical_geneticist_licence
-        #
-        # other config params (eg. date of CGI draft) retain original values from JSON
-        supplement = data[cc.CONFIG][self.SUPPLEMENT]
-        for key in mdc_supplement.keys():
-            supplement[key] = mdc_supplement.get(key)
-        return supplement
-
     def has_summary(self, data):
-        # check if summary plugin is present
+        # some reports do not have a summary, eg. PWGS
         return self.SUMMARY in data[cc.PLUGINS]
-
-    def render(self, json_path, out_dir, pdf):
-        """
-        Simple rendering to HTML/PDF for an existing JSON file
-        """
-        with open(json_path) as in_file:
-            data = json.loads(in_file.read())
-        self.base_render(data, out_dir, pdf)
 
     def run(self, args):
         """
@@ -67,12 +47,6 @@ class main(main_base):
                 ap.get_out_dir(),
                 ap.get_json()
             )
-        elif mode == constants.RENDER:
-            self.render(
-                ap.get_json(),
-                ap.get_out_dir(),
-                ap.is_pdf_enabled()
-            )
         elif mode == constants.REPORT:
             self.report(
                 ap.get_json(),
@@ -81,15 +55,6 @@ class main(main_base):
                 ap.is_pdf_enabled(),
                 ap.get_ini_path(),
                 ap.get_summary_path()
-            )
-        elif mode == constants.UPDATE:
-            self.update(
-                ap.get_config_path(),
-                ap.get_json(),
-                ap.get_out_dir(),
-                ap.is_pdf_enabled(),
-                ap.is_write_json_enabled(),
-                ap.is_forced()
             )
         else:
             msg = "Mode '{0}' is not defined in Djerba mini.main!".format(mode)
@@ -114,25 +79,39 @@ class main(main_base):
             if ini_path:
                 self.validate_ini(ini_path)
                 config.read(ini_path)
+            # set the assay
+            try:
+                assay = data[cc.CONFIG][constants.SUPPLEMENTARY]['assay']
+            except KeyError as err:
+                msg = "Cannot find Djerba assay from input JSON: {0}".format(err)
+                self.logger.error(msg)
+                raise MiniDjerbaScriptError(msg) from err
+            config.set(constants.SUPPLEMENTARY, 'assay', assay)
             if summary_path:
-                config.add_section(constants.SUMMARY)
-                config.set(constants.SUMMARY, 'summary_file', summary_path)
+                if self.has_summary(data):
+                    config.add_section(constants.SUMMARY)
+                    config.set(constants.SUMMARY, 'summary_file', summary_path)
+                else:
+                    msg = "Mini-Djerba summary not supported for assay '{0}'".format(assay)
+                    self.logger.error(msg)
+                    raise MiniDjerbaScriptError(msg)
             # core params (eg. the CGI author name) are unchanged from the input JSON
             config.add_section(cc.CORE)
             core_params = data[cc.CONFIG][cc.CORE]
             for k in core_params.keys():
                 config.set(cc.CORE, k, core_params[k])
-            # set the assay, if needed
-            if config.has_section(constants.SUPPLEMENTARY):
-                assay = data[cc.CONFIG][constants.SUPPLEMENTARY]['assay']
-                config.set(constants.SUPPLEMENTARY, 'assay', assay)
+            # run configure_from_parser() to fill in defaults, and extract updated data
             new_data = self.base_extract(self.configure_from_parser(config))
             data = self.update_report_data(new_data, data, force)
-            # TODO write updated data as JSON to out_dir
-            self.logger.debug('Updated report JSON data from given config')
+            out_name = "{0}.updated.json".format(data[cc.CORE][cc.REPORT_ID])
+            out_path = os.path.join(out_dir, out_name)
+            with open(out_path, 'w', encoding=cc.TEXT_ENCODING) as out_file:
+                out_file.write(json.dumps(data))
+            self.logger.debug('Wrote updated JSON data to {0}'.format(out_path))
         else:
             self.logger.debug('No additional config given, rendering existing JSON')
         self.render_from_cache(data, doc_key, out_dir, pdf)
+        self.logger.info('Mini-Djerba report mode complete')
 
     def setup(self, out_dir, json_path):
         """
@@ -181,59 +160,7 @@ class main(main_base):
         with open(ini_path, 'w', encoding=cc.TEXT_ENCODING) as out_file:
             config.write(out_file)
         self.logger.debug('Wrote INI to '+ini_path)
-
-    def update(self, config_path, json_path, out_dir, pdf, write_json, force):
-        """
-        Differs from update method in core:
-        - MDC instead of INI/TXT as config
-        - No archive capability
-        - Always write (at least) HTML output
-        """
-        # read the config file and generate a ConfigParser on-the-fly
-        # write summary text to the workspace
-        # run configure step to populate default values
-        # then run extraction to get the data structure for update
-        self.logger.info("Updating {0} from {1}".format(json_path, config_path))
-        mdc_handler = mdc(self.log_level, self.log_path)
-        [patient_info, supplement_mdc, summary_text] = mdc_handler.read(config_path)
-        with open(json_path) as json_file:
-            old_json = json.loads(json_file.read())
-        config = ConfigParser()
-        # core params (eg. the CGI author name) are unchanged from the input JSON
-        config.add_section(cc.CORE)
-        core_params = old_json[cc.CONFIG][cc.CORE]
-        for k in core_params.keys():
-            config.set(cc.CORE, k, core_params[k])
-        # patient info params are all taken from the MDC file
-        self.logger.info("Updating patient info")
-        config.add_section(self.PATIENT_INFO)
-        for k in patient_info.keys():
-            config.set(self.PATIENT_INFO, k, patient_info[k])
-        config.add_section(self.SUPPLEMENT)
-        # supplementary params are a mix of MDC params and existing JSON defaults
-        supplement_params = self.get_supplement_params(supplement_mdc, old_json)
-        for k in supplement_params.keys():
-            config.set(self.SUPPLEMENT, k, str(supplement_params[k]))
-        if self.has_summary(old_json):
-            self.logger.info("Updating summary text")
-            config.add_section(self.SUMMARY)
-            summary_path = os.path.join(self.work_dir, self.SUMMARY_FILENAME)
-            with open(summary_path, 'w', encoding=cc.TEXT_ENCODING) as out_file:
-                print(summary_text, file=out_file)
-            config.set('summary', summary_plugin.SUMMARY_FILE, summary_path)
-        else:
-            # summary not required for PWGS
-            self.logger.info("Summary not present in input JSON, will not update")
-        config = self.configure_from_parser(config)
-        data_new = self.base_extract(config)
-        data = self.update_data_from_file(data_new, json_path, force)
-        self.base_render(data, out_dir, pdf)
-        if write_json:
-            json_path = os.path.join(out_dir, 'updated_report.json')
-            with open(json_path, 'w') as out_file:
-                print(json.dumps(data), file=out_file)
-            self.logger.info("Wrote updated JSON to "+json_path)
-        self.logger.info("Update complete")
+        self.logger.info('Mini-Djerba setup mode complete')
 
     def validate_ini(self, ini_path):
         """
