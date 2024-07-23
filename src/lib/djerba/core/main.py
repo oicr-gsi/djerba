c9239bf1cd9d3a2b28bb42c046550779b21c5c5c
iainrb <1695061+iainrb@users.noreply.github.com>
2024-07-23 15:57:39 -0400

    bugfix; correct html wrap call

--- src/lib/djerba/core/main.py
+++ src/lib/djerba/core/main.py
@@ -351,7 +351,7 @@ class main_base(core_base):
             # load the plugin and render HTML for cache update
             plugin = self.plugin_loader.load(plugin_name, self.workspace)
             raw_html = plugin.render(new_data[self.PLUGINS][plugin_name])
-            new_html[plugin_name] = self.html_cache.wrap_html(raw_html)
+            new_html[plugin_name] = self.html_cache.wrap_html(plugin_name, raw_html)
             self.logger.debug('Updated JSON for plugin {0}'.format(plugin_name))
         # now update the HTML cache; TODO support multiple doc types, eg. clinical/research
         doc_key = self._get_unique_doc_key(data)
