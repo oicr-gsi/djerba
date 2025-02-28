b56272562f583550f2d2253cdffbb5fe0b6d6c63
iainrb <1695061+iainrb@users.noreply.github.com>
2025-02-28 16:10:29 -0500

    Add test of FF case for tar.status

--- src/lib/djerba/plugins/tar/status/test/plugin_test.py
+++ src/lib/djerba/plugins/tar/status/test/plugin_test.py
@@ -1,13 +1,28 @@
 #! /usr/bin/env python3
 
+import os
+import unittest
+
 from djerba.plugins.plugin_tester import PluginTester
 from djerba.plugins.tar.status.plugin import main as status_plugin
 
-class TestTarSNVIndelPlugin(PluginTester):
+class TestTarStatus(PluginTester):
 
+    INI_NAME = 'status.ini'
 
     def test_status(self):
-        pass
+        test_source_dir = os.path.realpath(os.path.dirname(__file__))
+        
+        json_ff = os.path.join(test_source_dir, 'status_FF.json')
+                               
+        params = {
+            self.INI: self.INI_NAME,
+            self.JSON: json_ff,
+            self.MD5: '174901db896e2de1e4c48c96d0024109'
+        }
+        self.run_basic_test(test_source_dir, params)
 
+
+        
 if __name__ == '__main__':
     unittest.main()
