eb973c5e498b8ac8393d8aeaa096d3888b3b2edc
Aqsa Alam <aalam@oicr.on.ca>
2025-10-16 11:03:36 -0400

    Edited provenance code to pick up the filtered file and deleted normal maf constants

--- src/lib/djerba/helpers/provenance_helper/helper.py
+++ src/lib/djerba/helpers/provenance_helper/helper.py
@@ -51,6 +51,7 @@ class main(helper_base):
     WF_CONSENSUS_NORMAL = 'consensusCruncher_normal'
     WF_MAF_TUMOUR = 'maf_tumour'
     WF_MAF_NORMAL = 'maf_normal'
+    WF_MAF_FILTERED = 'maf_filtered' # tumour maf
     WF_ICHOR_JSON = 'metrics_json'
     WF_ICHOR_SEG = 'seg'
 
@@ -237,6 +238,7 @@ class main(helper_base):
             self.WF_CONSENSUS_NORMAL: reader.parse_tar_metrics_normal_path(),
             self.WF_MAF_TUMOUR: reader.parse_tar_maf_tumour_path(),
             self.WF_MAF_NORMAL: reader.parse_tar_maf_normal_path(),
+            self.WF_MAF_FILTERED: reader.parse_tar_maf_tumour_filtered_path(),
             self.WF_ICHOR_JSON: reader.parse_tar_ichorcna_json_path(),
             self.WF_ICHOR_SEG: reader.parse_tar_ichorcna_seg_path()
 
--- src/lib/djerba/plugins/tar/snv_indel/constants.py
+++ src/lib/djerba/plugins/tar/snv_indel/constants.py
@@ -10,9 +10,7 @@ CBIO_ID = 'cbio_id'
 TUMOUR_ID = 'tumour_id'
 NORMAL_ID = 'normal_id'
 MAF_FILE = 'maf_file' 
-MAF_NORMAL_FILE = 'maf_file_normal'
 WF_MAF = 'maf_tumour' 
-WF_MAF_NORMAL = 'maf_normal'
 
 FREQUENCY_FILE = "plugins/tar/snv_indel/data/TGL.frequency.20210609.annot.txt"
 GENES_TO_KEEP = ["BRCA2", 
